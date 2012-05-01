0909d391928b41a55f6cfbe3ee92e9229fc4073c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-05-01 10:34:35 +0200

    Removed the --force flag when building VPM to speed up building.

--- bin/vibe.cmd
+++ bin/vibe.cmd
@@ -2,19 +2,19 @@
 set LIBDIR=%~dp0..\lib\win-i386
 set BINDIR=%~dp0..\lib\bin
 set LIBS=ws2_32.lib %LIBDIR%\event2.lib %LIBDIR%\eay.lib %LIBDIR%\ssl.lib
-set DFLAGS=-debug -g -w -property --force
+set DFLAGS=-debug -g -w -property
 set EXEDIR=%TEMP%\.rdmd\source
 if NOT EXIST %EXEDIR% (
 	mkdir %EXEDIR%
 )
 copy %~dp0*.dll %EXEDIR% > nul 2>&1
 
-rem Update the application.
+rem Update the application using VPM.
 rdmd %DFLAGS% -of%EXEDIR%\vpm.exe -I%~dp0..\source %LIBS% %~dp0.\vpm.d
 
 rem Run the application.
 if EXIST deps.txt. (
-	rdmd %DFLAGS% -I%~dp0..\source -Jviews -Isource @deps.txt %LIBS% source\app.d %1 %2 %3 %4 %5 %6
+	rdmd --force %DFLAGS% -I%~dp0..\source -Jviews -Isource @deps.txt %LIBS% source\app.d %1 %2 %3 %4 %5 %6
 ) else (
-	rdmd %DFLAGS% -I%~dp0..\source -Jviews -Isource %LIBS% source\app.d %1 %2 %3 %4 %5 %6
+	rdmd --force %DFLAGS% -I%~dp0..\source -Jviews -Isource %LIBS% source\app.d %1 %2 %3 %4 %5 %6
 )
