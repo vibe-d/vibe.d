dca4f5bf4a0f3dc3897386fe09012acc39452fda
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-05-22 14:19:09 +0200

    Hotfix release

--- source/vibe/core/core.d
+++ source/vibe/core/core.d
@@ -173,7 +173,7 @@ bool isTaskLocalSet(string name)
 /**
 	A version string representing the current vibe version
 */
-enum VibeVersionString = "0.7.2";
+enum VibeVersionString = "0.7.3";
 
 
 /**************************************************************************************************/
