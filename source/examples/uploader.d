e5e1ddbf1f97898df3f7dfa5341814e5c3c0a52f
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-05-30 23:32:55 +0200

    Added an example for file uploading.

---
+++ source/examples/uploader.d
@@ -0,0 +1,23 @@
+import vibe.d;
+
+void uploadFile(HttpServerRequest req, HttpServerResponse res)
+{
+	auto pf = "file" in req.files;
+	enforce(pf !is null, "No file uploaded!");
+	moveFile(pf.tempPath, Path(".")~pf.filename);
+
+	res.writeBody("File uploaded!", "text/plain");
+}
+
+static this()
+{
+	setLogLevel(LogLevel.Debug);
+
+	auto router = new UrlRouter;
+	router.get("/", staticTemplate!"upload_form.dt");
+	router.post("/upload", &uploadFile);
+
+	auto settings = new HttpServerSettings;
+	settings.port = 8080;
+	listenHttp(settings, router);
+}
