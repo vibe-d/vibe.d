0bdf3b62b8679d14b00d818c3a6440fb208c5a0d
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-05-30 23:28:55 +0200

    Added createTempFile() and moveFile().

--- source/vibe/core/file.d
+++ source/vibe/core/file.d
@@ -8,14 +8,52 @@
 module vibe.core.file;
 
 public import vibe.core.driver;
+public import vibe.inet.url;
 
 import vibe.core.core;
+import vibe.core.log;
+
+import std.conv;
+import std.c.stdio;
+import std.file;
+import std.string;
 
 
-/** Opens a file stream with the specified mode.
+/**
+	Opens a file stream with the specified mode.
 */
+FileStream openFile(Path path, FileMode mode = FileMode.Read)
+{
+	return openFile(path.toNativeString(), mode);
+}
+/// ditto
 FileStream openFile(string path, FileMode mode = FileMode.Read)
 {
 	return getEventDriver().openFile(path, mode);
 }
 
+/**
+	Creates and opens a temporary file for writing.
+*/
+FileStream createTempFile()
+{
+	char[L_tmpnam] tmp;
+	tmpnam(tmp.ptr);
+	auto tmpname = to!string(tmp.ptr);
+	if( tmpname.startsWith("\\") ) tmpname = tmpname[1 .. $];
+	logDebug("tmp %s", tmp);
+	return openFile(tmpname, FileMode.CreateTrunc);
+}
+
+/**
+	Moves or renames a file.
+*/
+void moveFile(Path from, Path to)
+{
+	moveFile(from.toNativeString(), to.toNativeString());
+}
+//
+void moveFile(string from, string to)
+{
+	std.file.rename(from, to);
+}
