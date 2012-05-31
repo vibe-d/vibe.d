6d0be866454dc264947fa8f7b3df4246c7c2da28
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-05-31 23:40:10 +0200

    Fixed the makedocs.cmd script.

--- docs/makedocs.cmd
+++ docs/makedocs.cmd
@@ -1,6 +1,7 @@
 del docs.json
-rdmd --build-only -D -Dd. -X -Xfdocs.json -I..\source ..\source\vibe\d.d >nul 2>&1
+set FLAGS=..\lib\win-i386\event2.lib ..\lib\win-i386\eay.lib ..\lib\win-i386\ssl.lib ws2_32.lib
+rdmd --build-only -D -Dd. -X -Xfdocs.json -I..\source %FLAGS% ..\source\vibe\d.d >nul 2>&1
 del *.html
-rdmd -I..\source source\filter.d
-rdmd -g -version=JsonLineNumbers -I..\source source\docsteroids.d docs.json ddocs.json
+rdmd -I..\source %FLAGS% source\filter.d
+rdmd -g -version=JsonLineNumbers -I..\source %FLAGS% source\docsteroids.d docs.json ddocs.json
 copy ddocs.json ..\..\vibed.org\docs.json
