0295bc861d9d27f6fc30c7ac059362abdca9e5c2
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-06-05 10:07:18 +0200

    Adjusted the run_example.sh script (on Windows, not sure if it works yet).

--- bin/run_example.sh
+++ bin/run_example.sh
@@ -3,10 +3,9 @@ if [ "$1" = "" ]; then
 	echo "Usage: run_example (example name)"
 	echo ""
 	echo "Possible examples:"
-	for i in ../source/examples/*.d; do echo $i | sed "s/^[a-zA-Z._\/]*\//  /g; s/.d$//g"; done
+	for i in ../examples/*; do echo $i | sed "s/^[a-zA-Z._\/]*\//  /g"; done
 else
-	#use pkg-config or fallback to default flags
-	LIBS=$(pkg-config --libs libevent libevent_openssl ssl crypto 2>/dev/null || echo "-levent_openssl -levent -lssl -lcrypto")
-	LIBS=$(echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g')
-	rdmd -debug -g -gs -property -w -Jviews -I../source $LIBS ../source/examples/$1.d
+	pushd ../examples/$1
+	vibe
+	popd
 fi
