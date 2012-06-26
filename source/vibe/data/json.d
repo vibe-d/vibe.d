373d5c01f29f73a2bd30b51657748178e3940345
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-06-26 20:33:41 +0200

    Added parseJsonString() to allow for parsing strings without passing them as a reference.

--- source/vibe/data/json.d
+++ source/vibe/data/json.d
@@ -604,7 +604,10 @@ struct Json {
 /******************************************************************************/
 
 /**
-	Parses the given range as a JSON string and returns the corresponding JSON object.
+	Parses the given range as a JSON string and returns the corresponding Json object.
+
+	The range is shrunk during parsing, leaving any remaining text that is now part of
+	the JSON contents.
 
 	Throws an Exception if any parsing error occured.
 */
@@ -686,6 +689,18 @@ Json parseJson(R)(ref R range, int* line = null)
 	
 	assert(ret.type != Json.Type.Undefined);
 	version(JsonLineNumbers) ret.line = curline;
+	return ret;
+}
+
+/**
+	Parses the given JSON string and returns the corresponding Json object.
+	
+	Throws an Exception if any parsing error occurs.
+*/
+Json parseJsonString(string str)
+{
+	auto ret = parseJson(str);
+	enforce(str.strip().length == 0, "Expected end of string after JSON value.");
 	return ret;
 }
 
