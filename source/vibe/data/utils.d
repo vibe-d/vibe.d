98ad8a30e4f41de97d1b6a5cf36a383117325d8a
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-07-10 15:45:39 +0200

    Added a missing utility module - this will need a better place later.

---
+++ source/vibe/data/utils.d
@@ -0,0 +1,30 @@
+/**
+	Utility functions for data serialization
+
+	Copyright: © 2012 RejectedSoftware e.K.
+	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+	Authors: Sönke Ludwig
+*/
+module vibe.data.utils;
+
+public import std.traits;
+
+
+template isRWPlainField(T, string M)
+{
+	static if( !__traits(compiles, typeof(__traits(getMember, T, M))) ){
+		enum isRWPlainField = false;
+	} else {
+		//pragma(msg, T.stringof~"."~M~":"~typeof(__traits(getMember, T, M)).stringof);
+		enum isRWPlainField = isRWField!(T, M) && __traits(compiles, *(&__traits(getMember, Tgen!T(), M)) = *(&__traits(getMember, Tgen!T(), M)));
+	}
+}
+
+template isRWField(T, string M)
+{
+	enum isRWField = __traits(compiles, __traits(getMember, Tgen!T(), M) = __traits(getMember, Tgen!T(), M));
+	//pragma(msg, T.stringof~"."~M~": "~(isRWField?"1":"0"));
+}
+
+/// private
+private T Tgen(T)(){ return T.init; }
