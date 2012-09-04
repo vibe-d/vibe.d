75112d505930e62415505a47d002d29ba5a672dc
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-09-04 11:39:08 +0200

    Added a UDP send/receive example.

---
+++ examples/udp/source/app.d
@@ -0,0 +1,22 @@
+import vibe.d;
+
+static this()
+{
+	runTask({
+		auto udp_listener = listenUdp(1234);
+		while(true){
+			auto pack = udp_listener.recv();
+			logInfo("Got packet: %s", cast(string)pack);
+		}
+	});
+	
+	runTask({
+		auto udp_sender = listenUdp(0);
+		udp_sender.connect("127.0.0.1", 1234);
+		while(true){
+			sleep(dur!"msecs"(500));
+			logInfo("Sending packet...");
+			udp_sender.send(cast(ubyte[])"Hello, World!");
+		}
+	});
+}
