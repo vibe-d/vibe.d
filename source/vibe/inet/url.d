344fe34b930e70f1d76bd1e881e4ed97bd83289c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-04 00:34:55 +0200

    Put Path in a separate module and improved the documentation for both, Url and Path.

--- source/vibe/inet/url.d
+++ source/vibe/inet/url.d
@@ -7,6 +7,8 @@
 */
 module vibe.inet.url;
 
+public import vibe.inet.path;
+
 import vibe.textfilter.urlencode;
 
 import std.algorithm;
@@ -14,6 +16,7 @@ import std.array;
 import std.conv;
 import std.exception;
 import std.string;
+
 
 /**
 	Represents a URL decomposed into its components.
@@ -31,6 +34,7 @@ struct Url {
 		string m_anchor;
 	}
 
+	/// Constructs a new URL object from its components.
 	this(string schema, string host, ushort port, Path path)
 	{
 		m_schema = schema;
@@ -40,7 +44,10 @@ struct Url {
 		m_pathString = path.toString(true);
 	}
 
-	// TODO: additional validation required (e.g. valid host and user names and port)
+	/** Constructs a URL from its string representation.
+	
+		TODO: additional validation required (e.g. valid host and user names and port)
+	*/
 	static Url parse(string str)
 	{
 		Url ret;
@@ -116,19 +123,32 @@ struct Url {
 		m_pathString = pstr;
 	}
 
-	/// The host part of the URL
+	/// The host part of the URL (depends on the schema)
 	@property string host() const { return m_host; }
 	/// ditto
 	@property void host(string v) { m_host = v; }
 
+	/// The port part of the URL (optional)
 	@property ushort port() const { return m_port; }
+	/// ditto
 	@property port(ushort v) { m_port = v; }
+
+	/// The user name part of the URL (optional)
 	@property string username() const { return m_username; }
+	/// ditto
 	@property void username(string v) { m_username = v; }
+
+	/// The password part of the URL (optional)
 	@property string password() const { return m_password; }
+	/// ditto
 	@property void password(string v) { m_password = v; }
+
+	/// The query string part of the URL (optional)
 	@property string queryString() const { return m_queryString; }
+	/// ditto
 	@property void queryString(string v) { m_queryString = v; }
+
+	/// The anchor part of the URL (optional)
 	@property string anchor() const { return m_anchor; }
 
 	/// The path part plus query string and anchor
@@ -147,7 +167,7 @@ struct Url {
 		}
 		return str.data;
 	}
-
+	/// ditto
 	@property void localURI(string str)
 	{
 		auto ai = str.countUntil('#');
@@ -166,6 +186,7 @@ struct Url {
 		m_path = Path(urlDecode(str));
 	}
 
+	/// The URL to the parent path with query string and anchor stripped.
 	@property Url parentUrl() const {
 		Url ret;
 		ret.schema = schema;
@@ -177,7 +198,7 @@ struct Url {
 		return ret;
 	}
 
-
+	/// Converts this URL object to its string representation.
 	string toString()
 	const {
 		auto dst = appender!string();
@@ -241,257 +262,4 @@ unittest {
 	assert(url.path.toString() == "/sub2/index.html", url.path.toString());
 	assert(url.queryString == "query", url.queryString);
 	assert(url.anchor == "anchor", url.anchor);
-}
-
-/**
-	Represents an absolute or relative file system path.
-
-	This struct allows to do safe operations on paths, such as concatenation and sub paths. Checks
-	are done to disallow invalid operations such as concatenating two absolute paths. It also
-	validates path strings and allows for easy checking of malicious relative paths.
-*/
-struct Path {
-	private {
-		immutable(PathEntry)[] m_nodes;
-		bool m_absolute = false;
-		bool m_endsWithSlash = false;
-	}
-	
-	this(string pathstr)
-	{
-		m_nodes = cast(immutable)splitPath(pathstr);
-		m_absolute = (pathstr.startsWith("/") || m_nodes.length > 0 && m_nodes[0].toString().countUntil(':')>0);
-		m_endsWithSlash = pathstr.endsWith("/");
-		foreach( e; m_nodes ) assert(e.toString().length > 0);
-	}
-	
-	this(immutable(PathEntry)[] nodes, bool absolute)
-	{
-		m_nodes = nodes;
-		m_absolute = absolute;
-	}
-	
-	this(PathEntry entry){
-		m_nodes = [entry];
-		m_absolute = false;
-	}
-	
-	@property bool absolute() const { return m_absolute; }
-
-	void normalize()
-	{
-		immutable(PathEntry)[] newnodes;
-		foreach( n; m_nodes ){
-			switch(n.toString()){
-				default:
-					newnodes ~= n;
-					break;
-				case ".": break;
-				case "..":
-					enforce(!m_absolute || newnodes.length > 0, "Path goes below root node.");
-					if( newnodes.length > 0 && newnodes[$-1] != ".." ) newnodes = newnodes[0 .. $-1];
-					else newnodes ~= n;
-					break;
-			}
-		}
-		m_nodes = newnodes;
-	}
-	
-	string toString(bool in_url = false) const {
-		if( m_nodes.empty ) return absolute ? "/" : "";
-		
-		Appender!string ret;
-		
-		// for absolute unix paths start with /
-		if( in_url || absolute && !m_nodes[0].toString().endsWith(":") ) ret.put('/');
-		
-		foreach( i, f; m_nodes ){
-			if( i > 0 ) ret.put('/');
-			ret.put(f.toString());
-		}
-
-		if( m_nodes.length > 0 && m_endsWithSlash )
-			ret.put('/');
-		
-		return ret.data;
-	}
-	
-	string toNativeString() const {
-		Appender!string ret;
-		
-		// for absolute unix paths start with /
-		version(Posix) { if(absolute) ret.put('/'); }
-		
-		foreach( i, f; m_nodes ){
-			version(Windows) { if( i > 0 ) ret.put('\\'); }
-			version(Posix) { if( i > 0 ) ret.put('/'); }
-			else { enforce("Unsupported OS"); }
-			ret.put(f.toString());
-		}
-		
-		if( m_nodes.length > 0 && m_endsWithSlash ){
-			version(Windows) { ret.put('\\'); }
-			version(Posix) { ret.put('/'); }
-		}
-		
-		return ret.data;
-	}
-	
-	bool startsWith(const Path rhs) const {
-		if( rhs.m_nodes.length > m_nodes.length ) return false;
-		foreach( i; 0 .. rhs.m_nodes.length )
-			if( m_nodes[i] != rhs.m_nodes[i] )
-				return false;
-		return true;
-	}
-	
-	Path relativeTo(const Path parentPath) const {
-		int nup = 0;
-		while( parentPath.length > nup && !startsWith(parentPath[0 .. parentPath.length-nup]) ){
-			nup++;
-		}
-		Path ret = Path(null, false);
-		ret.m_endsWithSlash = true;
-		foreach( i; 0 .. nup ) ret ~= "..";
-		ret ~= Path(m_nodes[parentPath.length-nup .. $], false);
-		return ret;
-	}
-	
-	@property PathEntry head() const { enforce(m_nodes.length > 0); return m_nodes[$-1]; }
-	@property Path parentPath() const { return this[0 .. length-1]; }
-	@property immutable(PathEntry)[] nodes() const { return m_nodes; }
-	@property size_t length() const { return m_nodes.length; }
-	@property bool empty() const { return m_nodes.length == 0; }
-	@property bool endsWithSlash() const { return m_endsWithSlash; }
-	@property void endsWithSlash(bool v) { m_endsWithSlash = v; }
-
-	/// Determines if this path goes outside of its base path (i.e. begins with '..').
-	@property bool external() const { return !m_absolute && m_nodes.length > 0 && m_nodes[0].m_name == ".."; }
-		
-	PathEntry opIndex(size_t idx) const { return m_nodes[idx]; }
-	Path opSlice(size_t start, size_t end) const {
-		auto ret = Path(m_nodes[start .. end], start == 0 ? absolute : false);
-		if( end == m_nodes.length ) ret.m_endsWithSlash = m_endsWithSlash;
-		return ret;
-	}
-	size_t opDollar(int dim)() const if(dim == 0) { return m_nodes.length; }
-	
-	
-	Path opBinary(string OP)(const Path rhs) const if( OP == "~" ) {
-		Path ret;
-		ret.m_nodes = m_nodes;
-		ret.m_absolute = m_absolute;
-		ret.m_endsWithSlash = rhs.m_endsWithSlash;
-		
-		assert(!rhs.absolute);
-		size_t idx = m_nodes.length;
-		foreach(folder; rhs.m_nodes){
-			switch(folder.toString()){
-				default: ret.m_nodes = ret.m_nodes ~ folder; break;
-				case ".": break;
-				case "..":
-					enforce(!ret.absolute || ret.m_nodes.length > 0, "Relative path goes below root node!");
-					if( ret.m_nodes.length > 0 && ret.m_nodes[$-1].toString() != ".." )
-						ret.m_nodes = ret.m_nodes[0 .. $-1];
-					else ret.m_nodes = ret.m_nodes ~ folder;
-					break;
-			}
-		}
-		return ret;
-	}
-	
-	Path opBinary(string OP)(string rhs) const if( OP == "~" ) { assert(rhs.length > 0); return opBinary!"~"(Path(rhs)); }
-	Path opBinary(string OP)(PathEntry rhs) const if( OP == "~" ) { assert(rhs.toString().length > 0); return opBinary!"~"(Path(rhs)); }
-	void opOpAssign(string OP)(string rhs) if( OP == "~" ) { assert(rhs.length > 0); opOpAssign!"~"(Path(rhs)); }
-	void opOpAssign(string OP)(PathEntry rhs) if( OP == "~" ) { assert(rhs.toString().length > 0); opOpAssign!"~"(Path(rhs)); }
-	void opOpAssign(string OP)(Path rhs) if( OP == "~" ) { auto p = this ~ rhs; m_nodes = p.m_nodes; m_endsWithSlash = rhs.m_endsWithSlash; }
-	
-	bool opEquals(ref const Path rhs) const {
-		if( m_absolute != rhs.m_absolute ) return false;
-		if( m_endsWithSlash != rhs.m_endsWithSlash ) return false;
-		if( m_nodes.length != rhs.length ) return false;
-		foreach( i; 0 .. m_nodes.length )
-			if( m_nodes[i] != rhs.m_nodes[i] )
-				return false;
-		return true;
-	}
-
-	int opCmp(ref const Path rhs) const {
-		if( m_absolute != rhs.m_absolute ) return cast(int)m_absolute - cast(int)rhs.m_absolute;
-		if( m_nodes.length != rhs.length ) return false;
-		foreach( i; 0 .. min(m_nodes.length, rhs.m_nodes.length) )
-			if( m_nodes[i] != rhs.m_nodes[i] )
-				return m_nodes[i].opCmp(rhs.m_nodes[i]);
-		if( m_nodes.length > rhs.m_nodes.length ) return 1;
-		if( m_nodes.length < rhs.m_nodes.length ) return -1;
-		return 0;
-	}
-}
-
-struct PathEntry {
-	private {
-		string m_name;
-	}
-	
-	this(string str)
-	{
-		assert(str.countUntil('/') < 0 && str.countUntil('\\') < 0);
-		m_name = str;
-	}
-	
-	string toString() const { return m_name; }
-
-	Path opBinary(string OP)(PathEntry rhs) const if( OP == "~" ) { return Path(cast(immutable)[this, rhs], false); }
-	
-	bool opEquals(ref const PathEntry rhs) const { return m_name == rhs.m_name; }
-	bool opEquals(string rhs) const { return m_name == rhs; }
-	int opCmp(ref const PathEntry rhs) const { return m_name.cmp(rhs.m_name); }
-	int opCmp(string rhs) const { return m_name.cmp(rhs); }
-}
-
-private bool isValidFilename(string str)
-{
-	foreach( ch; str )
-		if( ch == '/' || /*ch == ':' ||*/ ch == '\\' ) return false;
-	return true;
-}
-
-/// Joins two path strings. subpath must be relative.
-string joinPath(string basepath, string subpath)
-{
-	Path p1 = Path(basepath);
-	Path p2 = Path(subpath);
-	return (p1 ~ p2).toString();
-}
-
-/// Splits up a path string into its elements/folders
-PathEntry[] splitPath(string path)
-{
-	if( path.startsWith("/") || path.startsWith("\\") ) path = path[1 .. $];
-	if( path.empty ) return null;
-	if( path.endsWith("/") || path.endsWith("\\") ) path = path[0 .. $-1];
-
-	// count the number of path nodes
-	size_t nelements = 0;
-	foreach( i, char ch; path )
-		if( ch == '\\' || ch == '/' )
-			nelements++;
-	nelements++;
-	
-	// reserve space for the elements
-	auto elements = new PathEntry[nelements];
-
-	// read and return the elements
-	size_t startidx = 0;
-	size_t eidx = 0;
-	foreach( i, char ch; path )
-		if( ch == '\\' || ch == '/' ){
-			enforce(i - startidx > 0, "Empty path entries not allowed.");
-			elements[eidx++] = PathEntry(path[startidx .. i]);
-			startidx = i+1;
-		}
-	elements[eidx++] = PathEntry(path[startidx .. $]);
-	enforce(path.length - startidx > 0, "Empty path entries not allowed.");
-	assert(eidx == nelements);
-	return elements;
 }
