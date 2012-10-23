c85afee45107d7aedbe4139df1e146a0b1b1490b
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-23 10:20:33 +0200

    Moved the example runner scripts to the root because the bin directory might disappear soon.

--- run-example.cmd
+++ run-example.cmd
@@ -3,12 +3,12 @@ if "%1"=="" (
 	echo Usage: run_example ^(example name^)
 	echo.
 	echo Possible examples:
-	pushd ..\examples
+	pushd examples
 	for /F "usebackq" %%i in (`dir /b`) do echo %%i
 	popd
 ) else (
 	echo Running %1...
-	pushd ..\examples\%1
+	pushd examples\%1
 	vibe
 	popd
 )
