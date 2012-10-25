960fa823ddd84d8cadf298b25929a69ee63ec000
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-25 18:25:13 +0200

    Fixed for a triggered assertion in LimitedInputStream.

--- source/vibe/http/client.d
+++ source/vibe/http/client.d
@@ -174,7 +174,7 @@ class HttpClient : EventedObject {
 
 		// prepare body the reader
 		if( req.method == HttpMethod.HEAD ){
-			res.bodyReader = new LimitedInputStream(null, 0);
+			res.bodyReader = new LimitedInputStream(m_stream, 0);
 		} else {
 			if( auto pte = "Transfer-Encoding" in res.headers ){
 				enforce(*pte == "chunked");
@@ -186,7 +186,7 @@ class HttpClient : EventedObject {
 			} else if( res.httpVersion == HttpVersion.HTTP_1_0 ){
 				res.bodyReader = m_stream;
 			}
-			if( !res.bodyReader ) res.bodyReader = new LimitedInputStream(null, 0);
+			if( !res.bodyReader ) res.bodyReader = new LimitedInputStream(m_stream, 0);
 			
 			// TODO: handle content-encoding: deflate, gzip
 		}
