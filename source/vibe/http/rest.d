69aadfd1cdded4ce0d3a0b8d8ff109a9c5f6cc63
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-01-19 09:49:01 +0100

    Removed left-over compile time message.

--- source/vibe/http/rest.d
+++ source/vibe/http/rest.d
@@ -771,7 +771,6 @@ unittest
 	// the following tests fail on DMD < 2.061 - instanceOf was added in 2.061 and we
 	// use this fact to avoid failing on DMD 2.060
 	static if( __traits(compiles, { assert(isInstanceOf!(Appender, Appender!string)); }) ){
-		pragma(msg, "Runing additional tests");
 		static assert(fullyQualifiedTypeName!(QualifiedNameTests.Inner)
 			== "vibe.http.rest.QualifiedNameTests.Inner");
 		static assert(fullyQualifiedTypeName!(ReturnType!(QualifiedNameTests.func))
