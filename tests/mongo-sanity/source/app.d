bb820c40db4267e3e7570d08f11571f29c1396c4
Dicebot <m.strashun@gmail.com>
2013-01-27 17:13:52 +0200

    Minimal mongo driver sanity testing project. Should be adapted upon real CI introduction

---
+++ tests/mongo-sanity/source/app.d
@@ -0,0 +1,26 @@
+import vibe.d;
+
+// Requires mongo service running on localhost with default port
+// Uses test database
+
+static this()
+{ 
+    auto client = connectMongoDB("localhost");
+    auto coll = client.getCollection("test.collection");
+    assert(coll.database.getLastError().code < 0);
+    assert(coll.name == "collection");
+    assert(coll.database.name == "test");
+    coll.remove();
+    coll.insert([ "key1" : "value1", "key2" : "value2"]);
+    coll.update(["key1" : "value1"], [ "key1" : "value1", "key2" : "1337"]);
+    assert(coll.database.getLastError().n == 1);
+    auto data = coll.find(["key1" : true]);
+    foreach (doc; data)
+    {
+        assert(doc.length == 1);
+        assert(doc.key2.get!string() == "1337");
+    }
+    coll.database.fsync();
+    auto logBson = client.getDB("admin").getLog("global");
+    assert(!logBson.isNull());
+}
