8e8921e71c60f46d17b4cab2f89b98d4638745e6
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-02-26 10:09:10 +0100

    Added a simple application to test vibe.core.concurrency.

---
+++ examples/message/source/app.d
@@ -0,0 +1,51 @@
+import vibe.d;
+
+static this()
+{
+	auto t1 = runTask({
+		while(true){
+			logDebug("receive1");
+			receive(
+				(string msg){
+					logInfo("Received string message: %s", msg);
+				},
+				(int msg){
+					logInfo("Received int message: %s", msg);
+				});
+			logDebug("receive2");
+			receive(
+				(double msg){
+					logInfo("Received double: %s", msg);
+				},
+				(int a, int b, int c){
+					logInfo("Received iii: %s %s %s", a, b, c);
+				});
+		}
+	});
+
+	auto t2 = runTask({
+		sleep(1.seconds());
+		logInfo("send Hello World");
+		t1.send("Hello, World!");
+		
+		sleep(1.seconds());
+		logInfo("send int 1");
+		t1.send(1);
+
+		sleep(1.seconds());
+		logInfo("send double 1.2");
+		t1.send(1.2);
+		
+		sleep(1.seconds());
+		logInfo("send int 2");
+		t1.send(2);
+		
+		sleep(1.seconds());
+		logInfo("send 3xint 1 2 3");
+		t1.send(1, 2, 3);
+		
+		sleep(1.seconds());
+		logInfo("send string Bye bye");
+		t1.send("Bye bye");
+	});
+}
