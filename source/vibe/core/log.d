949201db1691539b5a0a6f03faa8a281edbd4be0
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-03-22 19:08:06 +0100

    Add LogLevel.critical and HtmlLogger.

--- source/vibe/core/log.d
+++ source/vibe/core/log.d
@@ -58,6 +58,7 @@ void registerLogger(shared(Logger) logger)
 */
 void log(LogLevel level, /*string mod = __MODULE__, string func = __FUNCTION__,*/ string file = __FILE__, int line = __LINE__, T...)(string fmt, auto ref T args)
 nothrow {
+	static assert(level != LogLevel.none);
 	if (level < ss_minLevel || ss_loggers.empty) return;
 	try {
 		auto app = appender!string();
@@ -87,7 +88,7 @@ void logError(/*string mod = __MODULE__, string func = __FUNCTION__,*/ string fi
 void logCritical(/*string mod = __MODULE__, string func = __FUNCTION__,*/ string file = __FILE__, int line = __LINE__, T...)(string fmt, auto ref T args) nothrow { log!(LogLevel.critical/*, mod, func*/, file, line)(fmt, args); }
 
 
-void rawLog(/*string mod, string func,*/ string file, int line, LogLevel level, string text)
+private void rawLog(/*string mod, string func,*/ string file, int line, LogLevel level, string text)
 nothrow {
 	static uint makeid(void* ptr) { return (cast(ulong)ptr & 0xFFFFFFFF) ^ (cast(ulong)ptr >> 32); }
 
@@ -122,6 +123,7 @@ enum LogLevel {
 	info,
 	warn,
 	error,
+	critical,
 	fatal,
 	none,
 
@@ -136,7 +138,7 @@ enum LogLevel {
 	/// deprecated
 	Error = error,
 	/// deprecated
-	Fatal = fatal,
+	Critical = critical,
 	/// deprecated
 	None = none
 }
@@ -148,6 +150,7 @@ struct LogLine {
 	int line;
 	LogLevel level;
 	Thread thread;
+	string threadName;
 	uint threadID;
 	Fiber fiber;
 	uint fiberID;
@@ -156,7 +159,7 @@ struct LogLine {
 }
 
 class Logger {
-	abstract void log(in ref LogLine message);
+	abstract void log(ref LogLine message);
 }
 
 class FileLogger : Logger {
@@ -186,7 +189,7 @@ class FileLogger : Logger {
 		m_diagFile = m_infoFile;
 	}
 
-	override void log(in ref LogLine msg)
+	override void log(ref LogLine msg)
 	{
 		if (msg.level < this.minLevel) return;
 
@@ -200,6 +203,7 @@ class FileLogger : Logger {
 			case LogLevel.info: pref = "INF"; file = m_infoFile; break;
 			case LogLevel.warn: pref = "WRN"; file = m_diagFile; break;
 			case LogLevel.error: pref = "ERR"; file = m_diagFile; break;
+			case LogLevel.critical: pref = "CRITICAL"; file = m_diagFile; break;
 			case LogLevel.fatal: pref = "FATAL"; file = m_diagFile; break;
 			case LogLevel.none: assert(false);
 		}
@@ -219,6 +223,152 @@ class FileLogger : Logger {
 	}
 }
 
+
+class HtmlLogger : Logger {
+	private {
+		File m_logFile;
+	}
+
+	this(string filename = "log.html")
+	{
+		m_logFile = File(filename, "wt");
+		writeHeader();
+	}
+
+	~this()
+	{
+		//version(FinalizerDebug) writeln("HtmlLogWritet.~this");
+		writeFooter();
+		m_logFile.close();
+		//version(FinalizerDebug) writeln("HtmlLogWritet.~this out");
+	}
+
+	override void log(ref LogLine msg)
+	{
+		if( !m_logFile.isOpen ) return;
+
+		final switch (msg.level) {
+			case LogLevel.none: assert(false);
+			case LogLevel.verbose4: m_logFile.write(`<div class="verbose4">`); break;
+			case LogLevel.verbose3: m_logFile.write(`<div class="verbose3">`); break;
+			case LogLevel.verbose2: m_logFile.write(`<div class="verbose2">`); break;
+			case LogLevel.verbose1: m_logFile.write(`<div class="verbose1">`); break;
+			case LogLevel.info: m_logFile.write(`<div class="info">`); break;
+			case LogLevel.warn: m_logFile.write(`<div class="warn">`); break;
+			case LogLevel.error: m_logFile.write(`<div class="error">`); break;
+			case LogLevel.critical: m_logFile.write(`<div class="critical">`); break;
+			case LogLevel.fatal: m_logFile.write(`<div class="fatal">`); break;
+		}
+		m_logFile.writef(`<div class="timeStamp">%s</div>`, msg.time.toISOExtString());
+		m_logFile.writef(`<div class="threadName">%s</div>`, msg.thread.name);
+		m_logFile.writef(`<div class="message">%s</div>`, msg.text);
+		m_logFile.writeln(`</div>`);
+		m_logFile.flush();
+	}
+
+	private void writeHeader(){
+		if( !m_logFile.isOpen ) return;
+
+		m_logFile.writeln(
+`<html>
+<head>
+	<title>HTML Log</title>
+	<style content="text/css">
+		.verbose4 { position: relative; color: #E0E0E0; font-size: 9pt; }
+		.verbose3 { position: relative; color: #E0E0E0; font-size: 9pt; }
+		.verbose2 { position: relative; color: #E0E0E0; font-size: 9pt; }
+		.verbose1 { position: relative; color: #808080; }
+		.info { position: relative; color: black; }
+		.warn { position: relative; color: #E08000; }
+		.error { position: relative; color: red; }
+		.critical { position: relative; color: red; background-color: black; }
+		.fatal { position: relative; color: red; background-color: black; }
+
+		.log { margin-left: 10pt; }
+		.code {
+			font-family: "Courier New";
+			background-color: #F0F0F0;
+			border: 1px solid gray;
+			margin-bottom: 10pt;
+			margin-left: 30pt;
+			margin-right: 10pt;
+			padding-left: 0pt;
+		}
+
+		div.timeStamp {
+			position: absolute;
+			width: 50pt;
+		}
+		div.threadName {
+			position: absolute;
+			top: 0pt;
+			left: 50pt;
+			width: 100pt;
+		}
+		div.message {
+			position: relative;
+			top: 0pt;
+			left: 150pt;
+		}
+		body {
+			font-family: Tahoma, Arial, sans-serif;
+			font-size: 10pt;
+		}
+	</style>
+	<script language="JavaScript">
+		function enableStyle(i){
+			var style = document.styleSheets[0].cssRules[i].style;
+			style.visibility = "visible";
+			style.height = "";
+		}
+
+		function disableStyle(i){
+			var style = document.styleSheets[0].cssRules[i].style;
+			style.visibility = "hidden";
+			style.overflow = "hidden";
+			style.height = "0pt";
+		}
+
+		function updateLevels(){
+			var sel = document.getElementById("Level");
+			var level = sel.value;
+			for( i = 0; i < level; i++ ) disableStyle(i);
+			for( i = level; i < 5; i++ ) enableStyle(i);
+		}
+	</script>
+</head>
+<body style="padding: 0px; margin: 0px;" onLoad="updateLevels(); updateCode();">
+	<div style="position: fixed; z-index: 100; padding: 4pt; width:100%; background-color: lightgray; border-bottom: 1px solid black;">
+		<form style="margin: 0px;">
+			Minimum Log Level:
+			<select id="Level" onChange="updateLevels()">
+				<option value="0">Verbose 4</option>
+				<option value="1">Verbose 3</option>
+				<option value="2">Verbose 2</option>
+				<option value="3">Verbose 1</option>
+				<option value="4">Info</option>
+				<option value="5">Warn</option>
+				<option value="6">Error</option>
+				<option value="7">Critical</option>
+				<option value="8">Fatal</option>
+			</select>
+		</form>
+	</div>
+	<div style="height: 30pt;"></div>
+	<div class="log">`);
+		m_logFile.flush();
+	}
+
+	private void writeFooter(){
+		if( !m_logFile.isOpen ) return;
+
+		m_logFile.writeln(
+`	</div>
+</body>
+</html>`);
+		m_logFile.flush();
+	}
+}
 
 private {
 	shared LogLevel ss_minLevel = LogLevel.info;
