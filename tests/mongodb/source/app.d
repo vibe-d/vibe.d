af5247960e76c001d6a88e45ac951867d42fb960
Vladimir Panteleev <vladimir@thecybershadow.net>
2013-04-05 03:21:33 +0000

    Split up tests
    The old setup required that, for every component requiring a server
    (e.g. a DB driver), the user had ALL components installed and running
    on their development system (thus, wasn't very scalable).

---
+++ tests/mongodb/source/app.d
@@ -0,0 +1,45 @@
+/// Requires mongo service running on localhost with default port
+/// Uses test database
+
+module app;
+
+import vibe.vibe;
+
+void runTest()
+{
+	auto client = connectMongoDB("localhost");
+	auto coll = client.getCollection("test.collection");
+	assert(coll.database.getLastError().code < 0);
+	assert(coll.name == "collection");
+	assert(coll.database.name == "test");
+	coll.remove();
+	coll.insert([ "key1" : "value1", "key2" : "value2"]);
+	coll.update(["key1" : "value1"], [ "key1" : "value1", "key2" : "1337"]);
+	assert(coll.database.getLastError().n == 1);
+	auto data = coll.findOne(["key1" : "value1"]);
+	assert(!data.isNull());
+	assert(data.key2.get!string() == "1337");
+	coll.database.fsync();
+	auto logBson = client.getDatabase("admin").getLog("global");
+	assert(!logBson.isNull());
+
+	// testing cursor range interface
+	coll.remove();
+	coll.insert(["key1" : "value1"]);
+	coll.insert(["key1" : "value2"]);
+	coll.insert(["key1" : "value2"]);
+	auto data1 = coll.find(["key1" : "value1"]);
+	auto data2 = coll.find(["key1" : "value2"]);
+
+	import std.range;
+	auto converted = zip(data1, data2).map!( a => a[0].key1.get!string() ~ a[1].key1.get!string() )();
+	assert(!converted.empty);
+	assert(converted.front == "value1value2");
+}
+
+int main()
+{
+	setLogLevel(LogLevel.Debug);
+	runTask(toDelegate(&runTest));
+	return runEventLoop();
+}
