c0f36cdb25857891fe66c6fee2615112d7e6bd52
Vladimir Panteleev <vladimir@thecybershadow.net>
2013-04-13 20:18:27 +0000

    win32: Exit when out of events

--- source/vibe/core/drivers/win32.d
+++ source/vibe/core/drivers/win32.d
@@ -79,7 +79,7 @@ class Win32EventDriver : EventDriver {
 	int runEventLoop()
 	{
 		m_exit = false;
-		while( !m_exit )
+		while( !m_exit && haveEvents() )
 			runEventLoopOnce();
 		return 0;
 	}
@@ -110,6 +110,8 @@ class Win32EventDriver : EventDriver {
 		}
 		return 0;
 	}
+
+	private bool haveEvents() { return !m_fileWriters.byKey.empty || !m_socketHandlers.byKey.empty; }
 
 	private void waitForEvents(uint timeout)
 	{
