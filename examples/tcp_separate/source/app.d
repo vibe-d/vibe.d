63f4759a639bf778e64c2dc452e6df5d9a59d7a3
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-04-26 14:29:58 +0200

    Remove FullDuplexStream and adjust the tcp_separate example.

--- examples/tcp_separate/source/app.d
+++ examples/tcp_separate/source/app.d
@@ -12,28 +12,18 @@ shared static this()
 	// shows how to handle reading and writing of the TCP connection
 	// in separate tasks
 	listenTCP(7000, (conn){
-		// release ownership of the connection, so that other tasks
-		// can acquire it
-		conn.release();
-
 		bool quit = false;
 
 		auto wtask = runTask({
-			// acquire just the write end of the connection
-			auto writer = conn.acquireWriter();
-			scope(exit) conn.releaseWriter();
 			while (!quit && conn.connected) {
-				writer.write("Hello, World!\r\n");
+				conn.write("Hello, World!\r\n");
 				sleep(2.seconds());
 			}
 		});
 
 		auto rtask = runTask({
-			// acquire just the read end of the connection
-			auto reader = conn.acquireReader();
-			scope(exit) conn.releaseReader();
 			while (!quit && conn.connected) {
-				auto ln = cast(string)reader.readLine();
+				auto ln = cast(string)conn.readLine();
 				if (ln == "quit") quit = true;
 				else logInfo("Got line: %s", ln);
 			}
@@ -44,9 +34,7 @@ shared static this()
 		wtask.join();
 
 		// if the connection is still alive, reacquire and close it
-		if (conn.connected) {
-			conn.acquire();
+		if (conn.connected)
 			conn.close();
-		}
 	});
 }
