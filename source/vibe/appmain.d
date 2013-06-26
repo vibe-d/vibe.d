c98e28676f4481526cc6f448e0cbe97d51a1777a
Soenke Ludwig <sludwig@rejectedsoftware.com>
2013-06-26 10:07:41 +0200

    Improve documentation of vibe.appmain

--- source/vibe/appmain.d
+++ source/vibe/appmain.d
@@ -8,10 +8,11 @@
 	The application itself then just has to initialize itself from within a 'static this' module
 	constructor and perform the appropriate calls to listen for connections or other operations.
 
-	If you want to provide your own main() function, you have to import 'vibe.vibe' instead of
-	'vibe.d'. Be sure to call start() at the end of your main function in this case. Also beware
-	that any global configuration is not applied in this case and features such as priviledge
-	lowering are not in place.
+	If you want to provide your own main function, you have to import vibe.vibe instead of
+	vibe.d and define a -version=VibeCustomMain. Be sure to call vibe.core.core.runEventLoop
+	at the end of your main function in this case. Also beware that you have to make appropriate
+	calls to vibe.core.args.finalizeCommandLineOptions and vibe.core.core.lowerPrivileges to get the
+	same behavior. 
 
 	Copyright: © 2012 RejectedSoftware e.K.
 	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
