436bea9de0a70c5d84b1f15e15975fce95ec8a20
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-06-30 12:34:12 +0200

    Remove the deprecated vibe.stream.stream module and update package.json.

--- source/vibe/stream/stream.d
+++
@@ -1,6 +0,0 @@
-module vibe.stream.stream;
-
-public import vibe.core.stream;
-public import vibe.stream.operations;
-
-pragma(msg, "vibe.stream.stream has been split into vibe.core.stream and vibe.stream.operations and will be removed soon. Please update your imports.");
