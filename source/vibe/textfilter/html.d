275dc3fa385aa96b9feac81b191a1ee2637aeb8d
Martin Nowak <code@dawg.eu>
2013-09-27 06:52:19 +0200

    replace foreach(dchar; s) loop with explicit for+range
    - Using foreach (dchar; string) results in a runtime
      call to _aApplycd which is a less optimized decoding
      routine and invokes the foreach delegate for each dchar.

--- source/vibe/textfilter/html.d
+++ source/vibe/textfilter/html.d
@@ -32,8 +32,8 @@ string htmlEscape(string str)
 */
 void filterHTMLEscape(R)(ref R dst, string str)
 {
-	foreach( dchar ch; str )
-		filterHTMLEscape(dst, ch, HTMLEscapeFlags.escapeNewline);
+	for( ; !str.empty; str.popFront() )
+		filterHTMLEscape(dst, str.front, HTMLEscapeFlags.escapeNewline);
 }
 
 /// Deprecated compatibility alias
@@ -59,8 +59,8 @@ string htmlAttribEscape(string str)
 */
 void filterHTMLAttribEscape(R)(ref R dst, string str)
 {
-	foreach( dchar ch; str )
-		filterHTMLEscape(dst, ch, HTMLEscapeFlags.escapeNewline|HTMLEscapeFlags.escapeQuotes);
+	for( ; !str.empty; str.popFront() )
+		filterHTMLEscape(dst, str.front, HTMLEscapeFlags.escapeNewline|HTMLEscapeFlags.escapeQuotes);
 }
 
 /// Deprecated compatibility alias
@@ -86,9 +86,9 @@ string htmlAllEscape()(string str)
 */
 void filterHTMLAllEscape(R)(ref R dst, string str)
 {
-	foreach( dchar ch; str ){
+	for( ; !str.empty; str.popFront() ){
 		dst.put("&#");
-		dst.put(to!string(cast(int)ch)); 
+		dst.put(to!string(cast(int)str.front));
 		dst.put(';');
 	}
 }
@@ -103,8 +103,8 @@ deprecated("Please use filterHTMLAllEscape instead.") alias filterHtmlAllEscape 
 string htmlEscapeMin(string str)
 {
 	auto dst = appender!string();
-	foreach( dchar ch; str )
-		filterHTMLEscape(dst, ch, HTMLEscapeFlags.escapeMinimal);
+	for( ; !str.empty; str.popFront() )
+		filterHTMLEscape(dst, str.front, HTMLEscapeFlags.escapeMinimal);
 	return dst.data();
 }
 
