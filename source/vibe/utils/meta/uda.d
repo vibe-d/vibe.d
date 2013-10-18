110b2651944c08e94d1c893fca66aba2e7cd6211
japplegame <japplegame@gmail.com>
2013-10-18 18:19:28 +0400

    split unittests

--- source/vibe/utils/meta/uda.d
+++ source/vibe/utils/meta/uda.d
@@ -22,64 +22,78 @@ template extractUda(UDA, alias Symbol)
 {
 	import std.typetuple : TypeTuple;
 
-    private alias TypeTuple!(__traits(getAttributes, Symbol)) udaTuple;
+	private alias TypeTuple!(__traits(getAttributes, Symbol)) udaTuple;
 
-    private template extract(list...)
-    {
-        static if (!list.length)
-            enum extract = null;
-        else {
+	private template extract(list...)
+	{
+		static if (!list.length)
+			enum extract = null;
+		else {
 			static assert (!is(list[0] == UDA), "extractUda is designed to look up values, not types");
 
 			static if (is(typeof(list[0]) == UDA))
-            	enum extract = list[0];
-	        else
-    	        enum extract = extract!(list[1..$]);
+				enum extract = list[0];
+			else
+				enum extract = extract!(list[1..$]);
 		}
-    }
+	}
 
-    enum extractUda = extract!udaTuple;
-}
-
-/**
-    Determines index of UDA.
-
-    Params:
-        UDA = attribute to search
-        Symbol = symbol to query
-
-    Returns: index of attribute or -1 if attribute is not found
-*/
-template indexOfUda(alias UDA, alias Symbol) {
-    import std.typetuple : staticIndexOf;
-    enum indexOfUda = staticIndexOf!(UDA, __traits(getAttributes, Symbol));
-}
-
-/**
-    Determines whether Symbol has given UDA
-
-    Params:
-        UDA = attribute to search
-        Symbol = symbol to query
-
-    Returns: true if symbol has given UDA
-*/
-template hasUda(alias UDA, alias Symbol) {
-    enum bool hasUda = indexOfUda!(UDA, Symbol) != -1;
+	enum extractUda = extract!udaTuple;
 }
 
 ///
 unittest
 {
-    struct Attribute { int x; }
-    @("something", Attribute(42), Attribute(41)) void symbol();
-    static assert (extractUda!(string, symbol) == "something");
-    static assert (extractUda!(Attribute, symbol) == Attribute(42));
-    static assert (extractUda!(int, symbol) == null);
-    static assert(indexOfUda!("something", symbol) == 0);
-    static assert(indexOfUda!(Attribute(41), symbol) == 2);
-    static assert(indexOfUda!(43, symbol) == -1);
-    static assert(hasUda!("something", symbol));
-    static assert(hasUda!(Attribute(42), symbol));
-    static assert(!hasUda!(Attribute(44), symbol));
+	struct Attribute { int x; }
+	@("something", Attribute(42), Attribute(41)) void symbol();
+	static assert (extractUda!(string, symbol) == "something");
+	static assert (extractUda!(Attribute, symbol) == Attribute(42));
+	static assert (extractUda!(int, symbol) == null);
+}
+
+/**
+	Determines index of UDA.
+
+	Params:
+		UDA = attribute to search
+		Symbol = symbol to query
+
+	Returns: index of attribute or -1 if attribute is not found
+*/
+template indexOfUda(alias UDA, alias Symbol) {
+	import std.typetuple : staticIndexOf;
+	enum indexOfUda = staticIndexOf!(UDA, __traits(getAttributes, Symbol));
+}
+
+///
+unittest
+{
+	struct Attribute { int x; }
+	@("something", Attribute(42), Attribute(41)) void symbol();
+	static assert(indexOfUda!("something", symbol) == 0);
+	static assert(indexOfUda!(Attribute(41), symbol) == 2);
+	static assert(indexOfUda!(43, symbol) == -1);
+}
+
+/**
+	Determines whether Symbol has given UDA
+
+	Params:
+		UDA = attribute to search
+		Symbol = symbol to query
+
+	Returns: true if symbol has given UDA
+*/
+template hasUda(alias UDA, alias Symbol) {
+	enum bool hasUda = indexOfUda!(UDA, Symbol) != -1;
+}
+
+///
+unittest
+{
+	struct Attribute { int x; }
+	@("something", Attribute(42), Attribute(41)) void symbol();
+	static assert(hasUda!("something", symbol));
+	static assert(hasUda!(Attribute(42), symbol));
+	static assert(!hasUda!(Attribute(44), symbol));
 }
