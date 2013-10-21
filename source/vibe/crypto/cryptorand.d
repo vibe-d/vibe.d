a7b1d1fbb68dcfed812f79398b096742f843a36d
Ilya Stromberg <ilya-stromberg-2009@yandex.ru>
2013-10-21 18:45:41 +0400

    Add hash-based random number generator

--- source/vibe/crypto/cryptorand.d
+++ source/vibe/crypto/cryptorand.d
@@ -131,7 +131,7 @@ struct SystemRand
 	}
 	
 	//Return current buffer with random number
-	//Current buffer will be overwritten by the next call to popFront()
+	//Current buffer will be overwritten by the next call of popFront()
 	//For long-term storage copy array using .dup or.idup methods
 	@property ubyte[] front() nothrow
 	{
@@ -158,7 +158,6 @@ unittest
 {
 	import std.algorithm;
 	import std.range;
-	import std.stdio;
 	
 	//number random bytes in the buffer
 	enum uint bufferSize = 20;
@@ -196,5 +195,103 @@ unittest
 			//create the next random number
 			systemRand.popFront();
 		}
+	}
+}
+
+
+import std.digest.sha;
+
+//Hash-based cryptographically secure random number generator
+//Use only cryptographically secure hash functions like SHA-512 or SHA1, but not MD5
+struct HashRand(Hash)
+if(isDigest!Hash)
+{
+	//infinite range - always false
+	enum bool empty = false;
+	
+	ubyte[] buffer;
+	
+	//random number generator
+	SystemRand rand;
+	
+	@disable this();
+	
+	//Creates new random generator
+	//"factor" param shows in how many times a random number generator buffer bigger than output buffer
+	//Increase "factor" value if you need more security because it increases entropy level
+	//Decrease "factor" value if you need more speed
+	this(uint factor)
+	in
+	{
+		assert(factor, "factor must be larger than 0");
+	}
+	body
+	{
+		//create buffer
+		this.buffer = new ubyte[digestLength!Hash];
+		
+		//create random number generator
+		this.rand = SystemRand(factor * cast(uint)digestLength!Hash);
+		
+		//init buffer
+		this.popFront();
+	}
+	
+	//create a next random number
+	void popFront()
+	{
+		this.buffer[] = digest!Hash(this.rand.front);
+		this.rand.popFront();
+	}
+	
+	//Return current buffer with random number
+	//Current buffer will be overwritten by the next call of popFront()
+	//For long-term storage copy array using .dup or.idup methods
+	@property ubyte[] front() nothrow
+	{
+		return this.buffer;
+	}
+}
+
+alias HashRand!SHA1 SHA1HashRand;
+
+
+unittest
+{
+	import std.algorithm;
+	import std.range;
+	
+	//number random bytes in the buffer
+	enum uint bufferSize = digestLength!SHA1;
+	
+	//number of iteration counts 
+	enum iterationCount = 10;
+	
+	auto hashRand = SHA1HashRand(5);
+	
+	//must be a input range
+	static assert(isInputRange!SHA1HashRand);
+	//must return ubyte[]
+	static assert(is(typeof(hashRand.front) == ubyte[]));
+	
+	assert(!equal(hashRand.front, take(repeat(0), bufferSize)), "it's almost unbelievable - all random bytes is zero");
+	
+	//holds the previous random number after the creation of the next one
+	ubyte[] prevRadn = hashRand.front.dup;
+	//create the next random number
+	hashRand.popFront();
+	
+	//take "iterationCount" arrays with random bytes
+	foreach(i; 0..iterationCount)
+	{
+		assert(!equal(hashRand.front, take(repeat(0), bufferSize)), "it's almost unbelievable - all random bytes is zero");
+		
+		assert(!equal(hashRand.front, prevRadn), "it's almost unbelievable - current and previous random bytes are equal");
+		
+		//copy current random bytes for next iteration
+		prevRadn = hashRand.front.dup;
+		
+		//create the next random number
+		hashRand.popFront();
 	}
 }
