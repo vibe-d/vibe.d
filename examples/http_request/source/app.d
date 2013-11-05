1169c12e6eb476bcb9b3ea29addb8e02e1f022d6
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-11-05 08:15:57 +0100

    Use requestHTTP() for the HTTP request example. Fixes #374.

--- examples/http_request/source/app.d
+++ examples/http_request/source/app.d
@@ -4,17 +4,13 @@ import vibe.http.client;
 
 void main()
 {
-	auto client = new HTTPClient;
-	client.connect("www.google.com", 80);
-	
-	auto res = client.request((req){
-			req.url = "/";
-		});
-
-	logInfo("Response: %d", res.statusCode);
-	foreach (k, v; res.headers)
-		logInfo("Header: %s: %s", k, v);
-
-	(new NullOutputStream).write(res.bodyReader);
-	client.disconnect();
+	requestHTTP("http://www.google.com/",
+		(scope req) {
+		},
+		(scope res) {
+			logInfo("Response: %d", res.statusCode);
+			foreach (k, v; res.headers)
+				logInfo("Header: %s: %s", k, v);
+		}
+	);
 }
