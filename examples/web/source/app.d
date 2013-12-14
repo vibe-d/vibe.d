3d853714e32c4ecd18d5ff3b3dacf5ef25914a39
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-12-14 16:26:55 +0100

    Add a simple implementation of registerWebInterface along with an example package. See #427.

---
+++ examples/web/source/app.d
@@ -0,0 +1,87 @@
+module app;
+
+import std.exception : enforce;
+import vibe.core.log;
+import vibe.http.fileserver;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.utils.validation;
+import vibe.web.web;
+
+class UserSettings {
+	string userName;
+	bool someSetting;
+}
+
+class SampleService {
+	private {
+		SessionVar!(UserSettings, "settings") m_userSettings;
+	}
+
+	@path("/") void getHome()
+	{
+		auto settings = m_userSettings;
+		render!("home.dt", settings);
+	}
+
+	void getLogin(string error = null) { render!("login.dt", error); }
+	void postLogin(string user, string password)
+	{
+		try {
+			validateUserName(user);
+			enforce(password == "secret", "Invalid password.");
+		} catch (Exception e) {
+			getLogin(e.msg);
+			return;
+		}
+
+		auto s = new UserSettings;
+		s.userName = user;
+		s.someSetting = false;
+		m_userSettings = s;
+		redirect("./");
+	}
+
+	void postLogout(HTTPServerResponse res)
+	{
+		m_userSettings = null;
+		res.terminateSession();
+		redirect("./");
+	}
+
+	void getSettings(string error = null)
+	{
+		auto settings = m_userSettings;
+		render!("settings.dt", error, settings);
+	}
+
+	void postSettings(bool some_setting, string user_name)
+	{
+		try {
+			enforce(m_userSettings !is null, "Must be logged in to change settings.");
+			validateUserName(user_name);
+			m_userSettings.userName = user_name;
+			m_userSettings.someSetting = some_setting;
+		} catch (Exception e) {
+			getSettings(e.msg);
+			return;
+		}
+
+		redirect("./");
+	}
+}
+
+shared static this()
+{
+	auto router = new URLRouter;
+	router.registerWebInterface(new SampleService);
+	router.get("*", serveStaticFiles("public/"));
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.sessionStore = new MemorySessionStore;
+	listenHTTP(settings, router);
+
+	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+}
---
+++ source/vibe/web/web.d
@@ -0,0 +1,155 @@
+/**
+	Implements a descriptive framework for building web interfaces.
+
+	Copyright: © 2013 RejectedSoftware e.K.
+	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+	Authors: Sönke Ludwig
+*/
+module vibe.web.web;
+
+public import vibe.internal.meta.funcattr : before, after;
+public import vibe.web.common;
+
+import vibe.core.core;
+import vibe.http.common;
+import vibe.http.router;
+import vibe.http.server;
+
+
+/** Registers a HTTP/web interface based on a class instance.
+
+	Each method corresponds to one or multiple request URLs.
+
+	...
+*/
+void registerWebInterface(C : Object, MethodStyle method_style = MethodStyle.lowerUnderscored)(URLRouter router, C instance, WebInterfaceSettings settings = null)
+{
+	import std.traits;
+
+	if (!settings) settings = new WebInterfaceSettings;
+
+	foreach (M; __traits(allMembers, C)) {
+		/*static if (isInstanceOf!(SessionVar, __traits(getMember, instance, M))) {
+			__traits(getMember, instance, M).m_getContext = toDelegate({ return s_requestContext; });
+		}*/
+		static if (!is(typeof(__traits(getMember, Object, M)))) { // exclude Object's default methods and field
+			foreach (overload; MemberFunctionsTuple!(C, M)) {
+				enum minfo = extractHTTPMethodAndName!overload();
+				enum url = minfo.hadPathUDA ? minfo.url : adjustMethodStyle(minfo.url, method_style);
+
+				router.match(minfo.method, concatURL(settings.urlPrefix, url), (req, res) {
+					handleRequest!(M, overload)(req, res, instance, settings);
+				});
+			}
+		}
+	}
+}
+
+void render(string diet_file, ALIASES...)()
+{
+	assert(s_requestContext.req !is null, "render() used outside of a wen interface request!");
+	auto req = s_requestContext.req;
+	vibe.http.server.render!(diet_file, req, ALIASES)(s_requestContext.res);
+}
+
+void redirect(string path_or_url)
+{
+	import std.array : startsWith;
+
+	assert(s_requestContext.req !is null, "redirect() used outside of a wen interface request!");
+	with (s_requestContext) {
+		URL url;
+		if (path_or_url.startsWith("/")) {
+			url = req.fullURL;
+			url.path = Path(path_or_url);
+		} else if (path_or_url.canFind(":")) { // TODO: better URL recognition
+			url = URL(path_or_url);
+		} else {
+			if (req.fullURL.path.endsWithSlash) url = req.fullURL ~ Path(path_or_url);
+			else url = req.fullURL.parentURL ~ Path(path_or_url);
+		}
+		res.redirect(url);
+	}
+}
+
+class WebInterfaceSettings {
+	string urlPrefix = "/";
+}
+
+
+/**
+	Maps a web interface member variable to a session field.
+*/
+struct SessionVar(T, string name) {
+	private {
+		T m_initValue;
+	}
+
+	this(T init_val) { m_initValue = init_val; }
+
+	void opAssign(T new_value) { this.value = new_value; }
+
+	@property T value()
+	{
+		assert(s_requestContext.req !is null, "SessionVar used outside of a web interface request!");
+		with(s_requestContext) {
+			if (req.session && req.session.isKeySet(name))
+				return req.session.get!T(name);
+		}
+
+		return m_initValue;
+	}
+
+	@property void value(T new_value)
+	{
+		assert(s_requestContext.req !is null, "SessionVar used outside of a web interface request!");
+		with(s_requestContext) {
+			if (!req.session) req.session = res.startSession();
+			req.session.set(name, new_value);
+		}
+	}
+
+	alias value this;
+}
+
+private {
+	TaskLocal!RequestContext s_requestContext;
+}
+
+private struct RequestContext {
+	HTTPServerRequest req;
+	HTTPServerResponse res;
+}
+
+private void handleRequest(string M, alias overload, C)(HTTPServerRequest req, HTTPServerResponse res, C instance, WebInterfaceSettings settings)
+{
+	import std.array : startsWith;
+	import std.conv;
+	import std.traits;
+
+	alias RET = ReturnType!overload;
+	alias PARAMS = ParameterTypeTuple!overload;
+	alias default_values = ParameterDefaultValueTuple!overload;
+	enum param_names = [ParameterIdentifierTuple!overload];
+
+	s_requestContext = RequestContext(req, res);
+	PARAMS params;
+	foreach (i, PT; PARAMS) {
+		static if (is(PT == InputStream)) params[i] = req.bodyReader;
+		else static if (is(PT == HTTPServerRequest) || is(PT == HTTPRequest)) params[i] = req;
+		else static if (is(PT == HTTPServerResponse) || is(PT == HTTPResponse)) params[i] = res;
+		else static if (param_names[i].startsWith("_")) params[i] = req.params[param_names[i][1 .. $]].to!PT;
+		else static if (is(PT == bool)) params[i] = param_names[i] in req.form || param_names[i] in req.query;
+		else if (auto pv = param_names[i] in req.form) params[i] = (*pv).to!PT;
+		else if (auto pv = param_names[i] in req.query) params[i] = (*pv).to!PT;
+		else static if (!is(default_values[i] == void)) params[i] = default_values[i];
+		else enforceHTTP(false, HTTPStatus.badRequest, "Missing form/query field "~param_names[i]);
+	}
+
+	static if (is(RET : InputStream)) {
+		res.writeBody(__traits(getMember, instance, M)(params));
+	} else {
+		static assert(is(RET == void), "Only InputStream and void are supported as return types.");
+		__traits(getMember, instance, M)(params);
+	}
+}
