ca6fd00a0ec9f1feeb4020273159ea9c4dfa2210
Michael Eisendle <michael@eisendle.me>
2014-02-13 17:50:21 +0100

    Add Redis Pub/Sub async subscribe/unsubscribe support.

--- examples/redis-pubsub/source/app.d
+++ examples/redis-pubsub/source/app.d
@@ -1,4 +1,6 @@
+import core.time;
 import std.functional;
+import vibe.core.core;
 import vibe.core.log;
 import vibe.db.redis.redis;
 
@@ -13,8 +15,15 @@ shared static this()
 	auto subscriber = new RedisSubscriber(new RedisClient());
 
 	subscriber.subscribe("test1", "test2");
+	sleep(1.seconds()); // give subscribe a chance to do it's job
 	publisher.publish("test1", "Hello World!");
 	publisher.publish("test2", "Hello from Channel 2");
 
 	auto task = subscriber.listen(toDelegate(&printReply));
+
+	runTask({
+		subscriber.subscribe("test-fiber");
+		publisher.publish("test-fiber", "Hello from the Fiber!");
+		subscriber.unsubscribe();
+	});
 }
