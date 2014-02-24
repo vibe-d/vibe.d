f48d0534015cc077f304a1b608555bc30a32d477
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-02-24 16:47:06 +0100

    Modernize the "diet" example.

--- examples/diet/source/app.d
+++ examples/diet/source/app.d
@@ -6,11 +6,10 @@ void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
 	string local_var = "Hello, World!";
 	res.headers["Content-Type"] = "text/html";
 	
-	auto output = res.bodyWriter();
-	//parseDietFile!("diet.dt", req, local_var)(output);
-	res.renderCompat!("diet.dt",
-		HTTPServerRequest, "req",
-		string, "local_var")(req, local_var);
+	res.render!("diet.dt", req, local_var);
+	//res.renderCompat!("diet.dt",
+	//	HTTPServerRequest, "req",
+	//	string, "local_var")(req, local_var);
 }
 
 shared static this()
