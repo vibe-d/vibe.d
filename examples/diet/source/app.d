93469580beca4198b059940d5c06944bd4a052ef
Damian Ziemba <nazriel@dzfl.pl>
2014-02-25 16:36:56 +0100

    Modernize diet example + add text block example

--- examples/diet/source/app.d
+++ examples/diet/source/app.d
@@ -4,9 +4,10 @@ import vibe.http.server;
 void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
 {
 	string local_var = "Hello, World!";
+	bool is_admin = false;
 	res.headers["Content-Type"] = "text/html";
 	
-	res.render!("diet.dt", req, local_var);
+	res.render!("diet.dt", req, local_var, is_admin);
 	//res.renderCompat!("diet.dt",
 	//	HTTPServerRequest, "req",
 	//	string, "local_var")(req, local_var);
