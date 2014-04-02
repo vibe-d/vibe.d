vibe-d/vibe.d

commit 98024b4e6294035ea823dcf23f18f6343ec2540c

Emit idle event after Libevent2Driver.processEvents - fixes hanging tasks.
Tasks didn't run at all in applications that used only processEvents() and neither runEventLoop(), nor runEventLoopOnce().

source/vibe/core/drivers/libevent2.d
@@ -187,6 +187,7 @@ class Libevent2Driver : EventDriver {
 	{
 		event_base_loop(m_eventLoop, EVLOOP_NONBLOCK);
 		processTimers();
+		m_core.notifyIdle();
 		if (m_exit) {
 			m_exit = false;
 			return false;

commit 26daf63b7374396e3847f3b2ac9fef168b6de0d9

Fix possible stack overflow for notifyIdle<->processEvents.
Fixes a regression introduced by 98024b4e6294035ea823dcf23f18f6343ec2540c/e1d0ec476bcc7121da35d11e77841efb4512462c.

source/vibe/core/core.d
@@ -137,7 +137,9 @@ void exitEventLoop(bool shutdown_all_threads = false)
 */
 bool processEvents()
 {
-	return getEventDriver().processEvents();
+	if (!getEventDriver().processEvents()) return false;
+	s_core.notifyIdle();
+	return true;
 }
 
 /**
@@ -915,7 +917,7 @@ private class VibeDriverCore : DriverCore {
 
 			if (!s_yieldedTasks.empty) again = true;
 
-			if (again && !processEvents()) {
+			if (again && !getEventDriver().processEvents()) {
 				s_exitEventLoop = true;
 				return;
 			}

source/vibe/core/drivers/libev.d
@@ -92,7 +92,6 @@ class LibevDriver : EventDriver {
 	bool processEvents()
 	{
 		ev_run(m_loop, EVRUN_NOWAIT);
-		m_core.notifyIdle();
 		if (m_break) {
 			m_break = false;
 			return false;

source/vibe/core/drivers/libevent2.d
@@ -187,7 +187,6 @@ class Libevent2Driver : EventDriver {
 	{
 		event_base_loop(m_eventLoop, EVLOOP_NONBLOCK);
 		processTimers();
-		m_core.notifyIdle();
 		if (m_exit) {
 			m_exit = false;
 			return false;

source/vibe/core/drivers/win32.d
@@ -128,7 +128,7 @@ class Win32EventDriver : EventDriver {
 			//if (++cnt % 10 == 0) processTimers();
 		}
 
-		m_core.notifyIdle();
+		if (timeout != 0) m_core.notifyIdle();
 
 		return true;
 	}