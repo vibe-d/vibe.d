8adf5e482c98b50470badc249ea9eece6a9e4a47
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-05-09 23:03:54 +0200

    Implement support for array/struct parameters and add doc comments in vibe.web.web.

--- source/vibe/web/web.d
+++ source/vibe/web/web.d
@@ -18,7 +18,6 @@ import vibe.http.server;
 /*
 	TODO:
 		- conversion errors of path place holder parameters should result in 404
-		- support arrays and composite types as parameters by splitting them up into multiple form/query parameters
 		- support format patterns for redirect()
 		- add a way to specify response headers without explicit access to "res"
 		- support class/interface getter properties and register their methods as well
@@ -27,9 +26,12 @@ import vibe.http.server;
 */
 
 
-/** Registers a HTTP/web interface based on a class instance.
+/**
+	Registers a HTTP/web interface based on a class instance.
 
-	Each method corresponds to one or multiple request URLs.
+	Each public method corresponds to one or multiple request URLs.
+
+	Supported types...
 
 	...
 */
@@ -56,6 +58,76 @@ void registerWebInterface(C : Object, MethodStyle method_style = MethodStyle.low
 	}
 }
 
+
+/**
+	Gives an overview of the basic features. For more advanced use, see the
+	example in the "examples/web/" directory.
+*/
+unittest {
+	import vibe.http.router;
+	import vibe.http.server;
+	import vibe.web.web;
+
+	class WebService {
+		private {
+			SessionVar!(string, "login_user") m_loginUser;
+		}
+
+		@path("/")
+		void getIndex()
+		{
+			//render!("index.dt");
+		}
+
+		// automatically mapped to: POST /login
+		void postLogin(string username, string password)
+		{
+			enforceHTTP(username.length > 0, HTTPStatus.forbidden,
+				"User name must not be empty.");
+			enforceHTTP(password == "secret", HTTPStatus.forbidden,
+				"Invalid password.");
+			m_loginUser = username;
+			redirect("/profile");
+		}
+
+		// automatically mapped to: POST /logout
+		void postLogout()
+		{
+			terminateSession();
+			redirect("/");
+		}
+
+		// automatically mapped to: GET /profile
+		void getProfile()
+		{
+			enforceHTTP(m_loginUser.length > 0, HTTPStatus.forbidden,
+				"Must be logged in to access the profile.");
+			//render!("profile.dt")
+		}
+	}
+
+	void run()
+	{
+		auto router = new URLRouter;
+		router.registerWebInterface(new WebService);
+
+		auto settings = new HTTPServerSettings;
+		settings.port = 8080;
+		listenHTTP(settings, router);
+	}
+}
+
+
+/**
+	Renders a Diet template file to the current HTTP response.
+
+	This function is equivalent to vibe.http.server.render, but implicitly
+	writes the result to the response object of the currently processed
+	request.
+
+	Note that this may only be called from a function/method
+	registered using registerWebInterface.
+*/
 void render(string diet_file, ALIASES...)()
 {
 	assert(s_requestContext.req !is null, "render() used outside of a web interface request!");
@@ -63,25 +135,45 @@ void render(string diet_file, ALIASES...)()
 	vibe.http.server.render!(diet_file, req, ALIASES)(s_requestContext.res);
 }
 
-void redirect(string path_or_url)
+
+/**
+	Redirects to the given URL.
+
+	The URL may either be a full URL, including the protocol and server
+	portion, or it may be the local part of the URI (the path and an
+	optional query string). Finally, it may also be a relative path that is
+	combined with the path of the current request to yield an absolute
+	path.
+
+	Note that this may only be called from a function/method
+	registered using registerWebInterface.
+*/
+void redirect(string url)
 {
 	import std.algorithm : canFind, startsWith;
 
 	assert(s_requestContext.req !is null, "redirect() used outside of a web interface request!");
 	alias ctx = s_requestContext;
-	URL url;
-	if (path_or_url.startsWith("/")) {
-		url = ctx.req.fullURL;
-		url.localURI = path_or_url;
-	} else if (path_or_url.canFind(":")) { // TODO: better URL recognition
-		url = URL(path_or_url);
+	URL fullurl;
+	if (url.startsWith("/")) {
+		fullurl = ctx.req.fullURL;
+		fullurl.localURI = url;
+	} else if (url.canFind(":")) { // TODO: better URL recognition
+		fullurl = URL(url);
 	} else {
-		if (ctx.req.fullURL.path.endsWithSlash) url = ctx.req.fullURL ~ Path(path_or_url);
-		else url = ctx.req.fullURL.parentURL ~ Path(path_or_url);
+		if (ctx.req.fullURL.path.endsWithSlash) fullurl = ctx.req.fullURL ~ Path(url);
+		else fullurl = ctx.req.fullURL.parentURL ~ Path(url);
 	}
-	ctx.res.redirect(url);
+	ctx.res.redirect(fullurl);
 }
 
+
+/**
+	Terminates the currently active session (if any).
+
+	Note that this may only be called from a function/method
+	registered using registerWebInterface.
+*/
 void terminateSession()
 {
 	alias ctx = s_requestContext;
@@ -91,6 +183,10 @@ void terminateSession()
 	}
 }
 
+
+/**
+	Encapsulates settings used to customize the generated web interface.
+*/
 class WebInterfaceSettings {
 	string urlPrefix = "/";
 }
@@ -98,6 +194,13 @@ class WebInterfaceSettings {
 
 /**
 	Maps a web interface member variable to a session field.
+
+	Setting a SessionVar variable will implicitly start a session, if none
+	has been started, yet. The content of the variable will be stored in
+	the session store and is automatically serialized and deserialized.
+
+	Note that variables of type SessionVar must always be normal members of a
+	class that was registered using registerWebInterface.
 */
 struct SessionVar(T, string name) {
 	private {
@@ -128,6 +231,7 @@ struct SessionVar(T, string name) {
 
 	alias value this;
 }
+
 
 private {
 	TaskLocal!RequestContext s_requestContext;
@@ -161,10 +265,12 @@ private void handleRequest(string M, alias overload, C)(HTTPServerRequest req, H
 		} else static if (is(PT == bool)) {
 			params[i] = param_names[i] in req.form || param_names[i] in req.query;
 		} else {
-			if (auto pv = param_names[i] in req.form) params[i] = (*pv).convTo!PT;
-			else if (auto pv = param_names[i] in req.query) params[i] = (*pv).convTo!PT;
-			else static if (!is(default_values[i] == void)) params[i] = default_values[i];
-			else enforceHTTP(false, HTTPStatus.badRequest, "Missing form/query field "~param_names[i]);
+			static if (!is(default_values[i] == void)) {
+				if (!readParamRec(req, params[i], param_names[i], false))
+					params[i] = default_values[i];
+			} else {
+				readParamRec(req, params[i], param_names[i], true);
+			}
 		}
 	}
 
@@ -174,6 +280,33 @@ private void handleRequest(string M, alias overload, C)(HTTPServerRequest req, H
 		static assert(is(RET == void), "Only InputStream and void are supported as return types.");
 		__traits(getMember, instance, M)(params);
 	}
+}
+
+private bool readParamRec(T)(HTTPServerRequest req, ref T dst, string fieldname, bool required)
+{
+	import std.traits;
+	import std.string;
+	import vibe.data.serialization;
+
+	static if (isDynamicArray!T && !isSomeString!T) {
+		alias EL = typeof(T.init[0]);
+		size_t idx = 0;
+		while (true) {
+			EL el;
+			if (!readParamRec(req, el, format("%s_%s", fieldname, idx), false))
+				break;
+			dst ~= el;
+			idx++;
+		}
+	} else static if (is(T == struct) && !isStringSerializable!T) {
+		foreach (m; __traits(allMembers, T))
+			if (!readParamRec(req, __traits(getMember, dst, m), fieldname~"_"~m, required))
+				return false;
+	} else if (auto pv = fieldname in req.form) dst = (*pv).convTo!T;
+	else if (auto pv = fieldname in req.query) dst = (*pv).convTo!T;
+	else if (required) throw new HTTPStatusException(HTTPStatus.badRequest, "Missing parameter "~fieldname);
+	else return false;
+	return true;
 }
 
 private T convTo(T)(string str)
