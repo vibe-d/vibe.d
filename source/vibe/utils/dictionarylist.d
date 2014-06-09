816526e5dccb0a2f0ef947977786af356fa15ae2
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-06-09 09:32:25 +0200

    Add another DictionaryList serialization test.

--- source/vibe/utils/dictionarylist.d
+++ source/vibe/utils/dictionarylist.d
@@ -292,4 +292,13 @@ unittest {
 	auto app = appender!string();
 	serializeToJson(app, b);
 	assert(app.data == `[{"key":"a","value":1},{"key":"A","value":2}]`, app.data);
+
+	DictionaryList!(int, true, 2) c;
+	c.addField("a", 1);
+	c.addField("b", 2);
+	c.addField("a", 3);
+	c.remove("b");
+	auto appc = appender!string();
+	serializeToJson(appc, c);
+	assert(appc.data == `[{"key":"a","value":1},{"key":"a","value":3}]`, appc.data);
 }
