f3057356c1426d77886a105067dc57ccb1e3c513
Etienne Cimon <etcimon@gmail.com>
2014-07-29 14:15:24 -0400

    Added formEncode, urlEncode
    Cleaned up a bit
    
    Reorganized the file
    
    More bug fixes, added unit tests
    
    Changed separator to char
    
    Use parseURLEncodedForm instead of _String
    
    Rewrote documentation for public functions
    
    Fix unit test

--- source/vibe/inet/webform.d
+++ source/vibe/inet/webform.d
@@ -21,7 +21,17 @@ import std.string;
 
 
 /**
-	Parses the form given by content_type and body_reader.
+	Writes the form fields into a key-value of type $(D FormFields), parsed from the 
+	specified $(D InputStream) and using the corresponding Content-Type header. Parsing 
+	is gracefully aborted if the Content-Type header is unrelated.
+	
+	Params:
+		fields = The key-value map to which form fields must be written
+		files = The $(D FilePart)s mapped to the corresponding key in which details on 
+				transmitted files will be written to.
+		content_type = The value of the Content-Type HTTP header.
+		body_reader = A valid $(D InputSteram) data stream consumed by the parser.
+		max_line_length = The byte-sized maximum length of lines used as boundary delimitors in Multi-Part forms.
 */
 bool parseFormData(ref FormFields fields, ref FilePartFormFields files, string content_type, InputStream body_reader, size_t max_line_length)
 {
@@ -42,7 +52,9 @@ bool parseFormData(ref FormFields fields, ref FilePartFormFields files, string c
 }
 
 /**
-	Parses a url encoded form (query string format) and puts the key/value pairs into params.
+	Writes to the $(D FormFields) the key-value map associated to an
+	application/x-www-form-urlencoded MIME formatted string, 
+		ie. all '+' characters are considered as ' ' spaces.
 */
 void parseURLEncodedForm(string str, ref FormFields params)
 {
@@ -78,6 +90,10 @@ void parseURLEncodedForm(string str, ref FormFields params)
 	}
 }
 
+/**
+	This example demonstrates parsing using all known form separators, it builds
+	a key-value map into the destination $(D FormFields)
+*/
 unittest
 {
 	FormFields dst;
@@ -93,8 +109,16 @@ unittest
 /**
 	Parses a form in "multipart/form-data" format.
 
-	If any _files are contained in the form, they are written to temporary _files using
-	vibe.core.file.createTempFile and returned in the files field.
+	If any files are contained in the form, they are written to temporary files using
+	$(D vibe.core.file.createTempFile) and their details returned in the files field.
+
+	Params:
+		fields = The key-value map to which form fields must be written
+		files = The $(D FilePart)s mapped to the corresponding key in which details on 
+				transmitted files will be written to.
+		content_type = The value of the Content-Type HTTP header.
+		body_reader = A valid $(D InputSteram) data stream consumed by the parser.
+		max_line_length = The byte-sized maximum length of lines used as boundary delimitors in Multi-Part forms.
 */
 void parseMultiPartForm(ref FormFields fields, ref FilePartFormFields files,
 	string content_type, InputStream body_reader, size_t max_line_length)
@@ -111,6 +135,9 @@ void parseMultiPartForm(ref FormFields fields, ref FilePartFormFields files,
 alias FormFields = DictionaryList!(string, true, 16);
 alias FilePartFormFields = DictionaryList!(FilePart, true, 1);
 
+/**
+	A FilePart is the data structure for individual "multipart/form-data" parts according to RFC 1867 section 7.
+*/
 struct FilePart {
 	InetHeaderMap headers;
 	PathEntry filename;
@@ -159,4 +186,249 @@ private bool parseMultipartFormPart(InputStream stream, ref FormFields form, ref
 		form.addField(name, data);
 	}
 	return stream.readLine(max_line_length) != "--";
+}
+
+/** 
+	Writes to the $(D OutputRange) an application/x-www-form-urlencoded MIME formatted string, 
+	ie. all spaces ' ' are replaced by the '+' character
+
+	Params:
+		dst	= The destination $(D OutputRange) where the resulting string must be written to.
+		map	= An iterable key-value map iterable with $(D foreach(string key, string value; map)).
+		sep	= A valid form separator, common values are '&' or ';'
+*/
+import std.range : isOutputRange;
+import std.traits : ValueType, KeyType;
+void formEncode(R, T)(ref R dst, T map, char sep = '&')
+{
+	formEncodeImpl(dst, map, sep, true);
+}
+
+/**
+	The following example demonstrates the use of $(D formEncode) with a json map,
+	the ordering of keys will be preserved in $(D Bson) and $(D DictionaryList) objects only.
+ */
+unittest {
+	import std.array : Appender;
+	string[string] map;
+	map["numbers"] = "123456789";
+	map["spaces"] = "1 2 3 4 a b c d";
+
+	Appender!string app;
+	app.formEncode(map);
+	assert(app.data == "spaces=1+2+3+4+a+b+c+d&numbers=123456789");
+}
+
+/** 
+	Returns an application/x-www-form-urlencoded MIME formatted string, 
+	ie. all spaces ' ' are replaced by the '+' character
+
+	Params:
+		map = An iterable key-value map iterable with $(D foreach(string key, string value; map)).
+		sep = A valid form separator, common values are '&' or ';'
+*/
+string formEncode(T)(T map, char sep = '&')
+{
+	return formEncodeImpl(map, sep, true);
+}
+
+/**
+	Writes to the $(D OutputRange) an URL encoded string as specified in RFC 3986 section 2
+	
+	Params:
+		dst	= The destination $(D OutputRange) where the resulting string must be written to.
+		map	= An iterable key-value map iterable with $(D foreach(string key, string value; map)).
+*/
+void urlEncode(R, T)(ref R dst, T map)
+{
+	formEncodeImpl(dst, map, "&", false);
+}
+
+
+/**
+	Returns an URL encoded string as specified in RFC 3986 section 2
+	
+	Params:
+		map = An iterable key-value map iterable with $(D foreach(string key, string value; map)).
+*/
+string urlEncode(T)(T map)
+{
+	return formEncodeImpl(map, '&', false);
+}
+
+private string formEncodeImpl(T)(T map, char sep, bool form_encode)
+	if ((is(T.ValueType == string) ||
+	     (is(ValueType!T == string) && is(KeyType!T == string))) &&
+	    __traits(compiles, (){ foreach(k, ref v; map){} }()))
+{
+	import std.array : Appender;
+	Appender!string dst;
+	size_t len;
+	
+	foreach (key, ref value; map) {
+		len += key.length;
+		len += value.length;
+	}
+	
+	// characters will be expanded, better use more space the first time and avoid additional allocations
+	dst.reserve(len*2);
+	dst.formEncodeImpl(map, sep, form_encode);
+	return dst.data;
+}
+
+
+private string formEncodeImpl(T)(T map, char sep, bool form_encode)
+	if ((T.stringof == "Json" || T.stringof == "Bson") &&
+	    __traits(compiles, (){ foreach(ref string k, ref T v; map){} }()))
+{
+	import std.array : Appender;
+	Appender!string dst;
+	size_t len;
+	
+	foreach (ref string key, ref T value; map) {
+		len += key.length;
+		len += value.length;
+	}
+	
+	// characters will be expanded, better use more space the first time and avoid additional allocations
+	dst.reserve(len*2);
+	dst.formEncodeImpl(map, sep, form_encode);
+	return dst.data;
+}
+
+private void formEncodeImpl(R, T)(ref R dst, T map, char sep, bool form_encode)
+	if (isOutputRange!(R, string) && 
+	    (is(T.ValueType == string) ||
+	 (is(ValueType!T == string) && is(KeyType!T == string))) &&
+	    __traits(compiles, (){ foreach(k, ref v; map){} }()))
+{
+	bool flag;
+	
+	foreach (key, ref value; map) {
+		if (flag)
+			dst.put(sep);
+		else 
+			flag = true;
+		filterURLEncode(dst, key, null, form_encode);
+		dst.put("=");
+		filterURLEncode(dst, value, null, form_encode);
+	}
+}
+
+private void formEncodeImpl(R, T)(ref R dst, T map, char sep, bool form_encode)
+	if (isOutputRange!(R, string) && 
+	    (T.stringof == "Json" || T.stringof == "Bson") &&
+	    __traits(compiles, (){ foreach(ref string k, ref T v; map){} }()))
+{
+	bool flag;
+	
+	foreach (ref string key, ref T value; map) {
+		if (flag)
+			dst.put(sep);
+		else 
+			flag = true;
+		filterURLEncode(dst, key, null, form_encode);
+		dst.put("=");
+		if (value.type == T.Type.string)
+			filterURLEncode(dst, value.get!string, null, form_encode);
+		else {
+			static if (T.stringof == "Json")
+				filterURLEncode(dst, value.to!string, null, form_encode);
+			else
+				filterURLEncode(dst, value.toString(), null, form_encode);
+
+		}
+	}
+}
+
+unittest
+{
+	import vibe.utils.dictionarylist : DictionaryList;
+	import vibe.data.json : Json;
+	import vibe.data.bson : Bson;
+	
+	string[string] aaMap;
+	DictionaryList!string dlMap;
+	Json jsonMap = Json.emptyObject;
+	Bson bsonMap = Bson.emptyObject;
+	
+	aaMap["unicode"] = "╤╳";
+	aaMap["numbers"] = "123456789";
+	aaMap["spaces"] = "1 2 3 4 a b c d";
+	aaMap["slashes"] = "1/2/3/4/5";
+	aaMap["equals"] = "1=2=3=4=5=6=7";
+	aaMap["complex"] = "╤╳/=$$\"'1!2()'\"";
+	aaMap["╤╳"] = "1";
+	
+	
+	dlMap["unicode"] = "╤╳";
+	dlMap["numbers"] = "123456789";
+	dlMap["spaces"] = "1 2 3 4 a b c d";
+	dlMap["slashes"] = "1/2/3/4/5";
+	dlMap["equals"] = "1=2=3=4=5=6=7";
+	dlMap["complex"] = "╤╳/=$$\"'1!2()'\"";
+	dlMap["╤╳"] = "1";
+	
+	
+	jsonMap["unicode"] = "╤╳";
+	jsonMap["numbers"] = "123456789";
+	jsonMap["spaces"] = "1 2 3 4 a b c d";
+	jsonMap["slashes"] = "1/2/3/4/5";
+	jsonMap["equals"] = "1=2=3=4=5=6=7";
+	jsonMap["complex"] = "╤╳/=$$\"'1!2()'\"";
+	jsonMap["╤╳"] = "1";
+	
+	bsonMap["unicode"] = "╤╳";
+	bsonMap["numbers"] = "123456789";
+	bsonMap["spaces"] = "1 2 3 4 a b c d";
+	bsonMap["slashes"] = "1/2/3/4/5";
+	bsonMap["equals"] = "1=2=3=4=5=6=7";
+	bsonMap["complex"] = "╤╳/=$$\"'1!2()'\"";
+	bsonMap["╤╳"] = "1";
+	
+	assert(urlEncode(aaMap) == "complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&unicode=%E2%95%A4%E2%95%B3&spaces=1%202%203%204%20a%20b%20c%20d&numbers=123456789&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&%E2%95%A4%E2%95%B3=1");
+	assert(urlEncode(dlMap) == "unicode=%E2%95%A4%E2%95%B3&numbers=123456789&spaces=1%202%203%204%20a%20b%20c%20d&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&%E2%95%A4%E2%95%B3=1");
+	assert(urlEncode(jsonMap) == "%E2%95%A4%E2%95%B3=1&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&unicode=%E2%95%A4%E2%95%B3&spaces=1%202%203%204%20a%20b%20c%20d&numbers=123456789&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&slashes=1%2F2%2F3%2F4%2F5");
+	assert(urlEncode(bsonMap) == "unicode=%E2%95%A4%E2%95%B3&numbers=123456789&spaces=1%202%203%204%20a%20b%20c%20d&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&%E2%95%A4%E2%95%B3=1");
+	{
+		FormFields aaFields;
+		parseURLEncodedForm(urlEncode(aaMap), aaFields);
+		assert(urlEncode(aaMap) == urlEncode(aaFields));
+		
+		FormFields dlFields;
+		parseURLEncodedForm(urlEncode(dlMap), dlFields);
+		assert(urlEncode(dlMap) == urlEncode(dlFields));
+		
+		FormFields jsonFields;
+		parseURLEncodedForm(urlEncode(jsonMap), jsonFields);
+		assert(urlEncode(jsonMap) == urlEncode(jsonFields));
+		
+		FormFields bsonFields;
+		parseURLEncodedForm(urlEncode(bsonMap), bsonFields);
+		assert(urlEncode(bsonMap) == urlEncode(bsonFields));
+	}
+	
+	assert(formEncode(aaMap) == "complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&unicode=%E2%95%A4%E2%95%B3&spaces=1+2+3+4+a+b+c+d&numbers=123456789&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&%E2%95%A4%E2%95%B3=1");
+	assert(formEncode(dlMap) == "unicode=%E2%95%A4%E2%95%B3&numbers=123456789&spaces=1+2+3+4+a+b+c+d&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&%E2%95%A4%E2%95%B3=1");
+	assert(formEncode(jsonMap) == "%E2%95%A4%E2%95%B3=1&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&unicode=%E2%95%A4%E2%95%B3&spaces=1+2+3+4+a+b+c+d&numbers=123456789&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&slashes=1%2F2%2F3%2F4%2F5");
+	assert(formEncode(bsonMap) == "unicode=%E2%95%A4%E2%95%B3&numbers=123456789&spaces=1+2+3+4+a+b+c+d&slashes=1%2F2%2F3%2F4%2F5&equals=1%3D2%3D3%3D4%3D5%3D6%3D7&complex=%E2%95%A4%E2%95%B3%2F%3D%24%24%22%271%212%28%29%27%22&%E2%95%A4%E2%95%B3=1");
+	
+	{
+		FormFields aaFields;
+		parseURLEncodedForm(formEncode(aaMap), aaFields);
+		assert(formEncode(aaMap) == formEncode(aaFields));
+		
+		FormFields dlFields;
+		parseURLEncodedForm(formEncode(dlMap), dlFields);
+		assert(formEncode(dlMap) == formEncode(dlFields));
+		
+		FormFields jsonFields;
+		parseURLEncodedForm(formEncode(jsonMap), jsonFields);
+		assert(formEncode(jsonMap) == formEncode(jsonFields));
+		
+		FormFields bsonFields;
+		parseURLEncodedForm(formEncode(bsonMap), bsonFields);
+		assert(formEncode(bsonMap) == formEncode(bsonFields));
+	}
+
 }
