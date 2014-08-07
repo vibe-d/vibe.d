632cd7bee2a898c23569055702819e8d29e30f50
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-08-07 16:43:32 +0200

    Add assertions to the Redis test. See #770.

--- tests/redis/source/app.d
+++ tests/redis/source/app.d
@@ -29,35 +29,40 @@ void runTest()
 		db.setEX("test8", 1000, "test8");
 		db.setEX("test9", 1000, "test9");
 		db.setEX("test10", 1000, "0");
+		assert(db.get("test1") == "test1");
+		assert(db.get("test2") == "test2");
+		assert(db.get("test3") == "test3");
+		assert(db.get("test4") == "test4");
+		assert(db.get("test5") == "test5");
+		assert(db.get("test6") == "test6");
+		assert(db.get("test7") == "test7");
+		assert(db.get("test8") == "test8");
+		assert(db.get("test9") == "test9");
+		assert(db.get("test10") == "0");
+
 		db.del("saddTests");
 		db.sadd("saddTests", "item1");
 		db.sadd("saddTests", "item2");
+		assert(db.smembers("saddTests").array.sort.equal(["item1", "item2"]));
 
 
-		assert(db.get!string("test1") == "test1");
-		db.get!string("test2");
-		db.get!string("test3");
-		db.get!string("test4");
-		db.get!string("test5");
-		db.get!string("test6");
-		db.get!string("test7");
-		db.get!string("test8");
-		db.get!string("test9");
-		db.get!string("test10");
 		db.append("test1", "test1append");
 		db.append("test2", "test2append");
-		db.get!string("test1");
-		db.get!string("test2");
+		assert(db.get!string("test1") == "test1test1append");
+		assert(db.get!string("test2") == "test2test2append");
+		
 		db.incr("test10");
+		assert(db.get!long("test10") == 1);
 
 		db.del("test1", "test2","test3","test4","test5","test6","test7","test8","test9","test10");
+		db.del("saddTests");
 
 		db.srem("test1", "test1append");
 		db.srem("test2", "test2append");
 
-		db.smembers("test1");
-
-		db.smembers("test2");
+		assert(db.smembers("test1").empty);
+		assert(db.smembers("test2").empty);
+		assert(!db.smembers("test1").hasNext());
 	}
 	RedisSubscriber sub = new RedisSubscriber(redis);
 	import std.datetime;
