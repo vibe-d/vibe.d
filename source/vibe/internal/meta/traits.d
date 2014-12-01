f9b48da9b710517c6a3f3b16528843ac25696113
Martin Nowak <code@dawg.eu>
2014-12-01 19:28:49 +0100

    missing import of TypeTuple

--- source/vibe/internal/meta/traits.d
+++ source/vibe/internal/meta/traits.d
@@ -235,7 +235,7 @@ package T Tgen(T)(){ return T.init; }
 */
 template isPublicMember(T, string M)
 {
-	import std.algorithm;
+	import std.algorithm, std.typetuple : TypeTuple;
 
 	static if (!__traits(compiles, TypeTuple!(__traits(getMember, T, M)))) enum isPublicMember = false;
 	else {
