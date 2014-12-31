36858b2602c2f367d986b82eb71eb2777ae8a773
Luca Niccoli <l.niccoli@awtech.it>
2014-12-31 15:18:06 +0100

    Add HTTPServerSettings.webSocketPingInterval
    It will be made accessible to the websocket via HTTPServerRequest
    
    Signed-off-by: Luca Niccoli <l.niccoli@awtech.it>

--- source/vibe/http/server.d
+++ source/vibe/http/server.d
@@ -522,11 +522,19 @@ final class HTTPServerSettings {
 	*/
 	bool useCompressionIfPossible = false;
 
+
+	/** Interval between WebSocket ping frames.
+
+		The default value is 60 seconds; set to Duration.zero to disable pings.
+	*/
+	Duration webSocketPingInterval;// = dur!"seconds"(60);
+
 	this()
 	{
 		// need to use the contructor because the Ubuntu 13.10 GDC cannot CTFE dur()
 		maxRequestTime = 0.seconds;
 		keepAliveTimeout = 10.seconds;
+		webSocketPingInterval = 60.seconds;
 	}
 }
 
