3087d3ea1c020052715eecfd74b448c57bfa2648
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-03-05 14:12:28 +0100

    Fix compilation of LibevManualEvent.

--- source/vibe/core/drivers/libev.d
+++ source/vibe/core/drivers/libev.d
@@ -285,6 +285,7 @@ final class LibevManualEvent : ManualEvent {
 
 	void emit()
 	{
+		static if (__VERSION__ <= 2066) scope (failure) assert(false); // synchronized is not nothrow on DMD 2.066 and below
 		atomicOp!"+="(m_emitCount, 1);
 		synchronized (m_mutex) {
 			foreach (sl; m_waiters)
@@ -292,28 +293,11 @@ final class LibevManualEvent : ManualEvent {
 		}
 	}
 
-	void wait()
-	{
-		wait(m_emitCount);
-	}
-
-	int wait(int reference_emit_count)
-	{
-		assert(!amOwner());
-		acquire();
-		scope(exit) release();
-		auto ec = this.emitCount;
-		while( ec == reference_emit_count ){
-			LibevDriver.ms_core.yieldForEvent();
-			ec = this.emitCount;
-		}
-		return ec;
-	}
-
-	int wait(Duration timeout, int reference_emit_count)
-	{
-		assert(false, "Not implemented!");
-	}
+	void wait() { wait(m_emitCount); }
+	int wait(int reference_emit_count) { return doWait!true(reference_emit_count); }
+	int wait(Duration timeout, int reference_emit_count) { return doWait!true(timeout, reference_emit_count); }
+	int waitDeferThrow(int reference_emit_count) { return doWait!false(reference_emit_count); }
+	int waitDeferThrow(Duration timeout, int reference_emit_count) { return doWait!false(timeout, reference_emit_count); }
 
 	void acquire()
 	{
@@ -352,6 +336,27 @@ final class LibevManualEvent : ManualEvent {
 	}
 
 	@property int emitCount() const { return atomicLoad(m_emitCount); }
+
+	private int doWait(bool INTERRUPTIBLE)(int reference_emit_count)
+	{
+		static if (!INTERRUPTIBLE) scope (failure) assert(false); // some functions are still not annotated nothrow
+		assert(!amOwner());
+		acquire();
+		scope(exit) release();
+		auto ec = this.emitCount;
+		while( ec == reference_emit_count ){
+			static if (INTERRUPTIBLE) LibevDriver.ms_core.yieldForEvent();
+			else LibevDriver.ms_core.yieldForEventDeferThrow();
+			ec = this.emitCount;
+		}
+		return ec;
+	}
+
+	private int doWait(bool INTERRUPTIBLE)(Duration timeout, int reference_emit_count)
+	{
+		assert(false, "Not implemented!");
+	}
+
 
 	private static nothrow extern(C)
 	void onSignal(ev_loop_t* loop, ev_async* w, int revents)
