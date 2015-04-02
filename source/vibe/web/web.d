0596fbd5e5c199c4129c3e43bc80aab7814b2c1a
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-04-02 22:19:05 +0200

    Fix URL redirection for trailing slash mismatch in vibe.web.web.
    Fixes D-Programming-Language/dub-registry#106.

--- source/vibe/web/web.d
+++ source/vibe/web/web.d
@@ -9,7 +9,7 @@
 
 	See $(D registerWebInterface) for an overview of how the system works.
 
-	Copyright: © 2013-2014 RejectedSoftware e.K.
+	Copyright: © 2013-2015 RejectedSoftware e.K.
 	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
 	Authors: Sönke Ludwig
 */
@@ -146,11 +146,12 @@ void registerWebInterface(C : Object, MethodStyle method_style = MethodStyle.low
 					});
 					if (settings.ignoreTrailingSlash && !fullurl.endsWith("*")) {
 						auto m = fullurl.endsWith("/") ? fullurl[0 .. $-1] : fullurl ~ "/";
-						auto fullpath = Path(fullurl);
 						router.match(minfo.method, m, (req, res) {
 							static if (minfo.method == HTTPMethod.GET) {
 								URL redurl = req.fullURL;
-								redurl.path = fullpath;
+								auto redpath = redurl.path;
+								redpath.endsWithSlash = !redpath.endsWithSlash;
+								redurl.path = redpath;
 								res.redirect(redurl);
 							} else {
 								handleRequest!(M, overload)(req, res, instance, settings);
