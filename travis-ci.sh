e071c79d008e5b6a000598f44e30fd89b280d726
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-04-17 15:51:52 +0200

    Only build :core with explicit configuration for now.

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,16 +3,16 @@
 set -e -o pipefail
 
 dub build -b release --compiler=$DC --config=${VIBED_DRIVER=libevent}
-dub test --compiler=$DC --config=${VIBED_DRIVER=libevent}
-dub test :base --compiler=$DC --config=${VIBED_DRIVER}
-dub test :utils --compiler=$DC --config=${VIBED_DRIVER}
-dub test :data --compiler=$DC --config=${VIBED_DRIVER}
-dub test :mail --compiler=$DC --config=${VIBED_DRIVER}
-dub test :http --compiler=$DC --config=${VIBED_DRIVER}
-dub test :diet --compiler=$DC --config=${VIBED_DRIVER}
-dub test :web --compiler=$DC --config=${VIBED_DRIVER}
-dub test :mongodb --compiler=$DC --config=${VIBED_DRIVER}
-dub test :redis --compiler=$DC --config=${VIBED_DRIVER}
+dub test --compiler=$DC #--config=${VIBED_DRIVER=libevent}
+dub test :utils --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :data --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :core --compiler=$DC --config=${VIBED_DRIVER}
+dub test :mail --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :http --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :diet --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :web --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :mongodb --compiler=$DC #--config=${VIBED_DRIVER}
+dub test :redis --compiler=$DC #--config=${VIBED_DRIVER}
 
 if [ ${BUILD_EXAMPLE=1} -eq 1 ]; then
     for ex in $(\ls -1 examples/); do
