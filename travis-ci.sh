4eeae9d63a0b3b025034870c5a0ad0db2f9a2878
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-04-17 15:29:46 +0200

    Split up vibe-d:base into :utils and :core.

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,6 +5,8 @@ set -e -o pipefail
 dub build -b release --compiler=$DC --config=${VIBED_DRIVER=libevent}
 dub test --compiler=$DC --config=${VIBED_DRIVER=libevent}
 dub test :base --compiler=$DC --config=${VIBED_DRIVER}
+dub test :utils --compiler=$DC --config=${VIBED_DRIVER}
+dub test :data --compiler=$DC --config=${VIBED_DRIVER}
 dub test :mail --compiler=$DC --config=${VIBED_DRIVER}
 dub test :http --compiler=$DC --config=${VIBED_DRIVER}
 dub test :diet --compiler=$DC --config=${VIBED_DRIVER}
