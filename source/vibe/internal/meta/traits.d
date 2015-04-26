9457225dd326e5edf6b3ee61873bc04a337bd38e
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-04-26 08:59:26 +0200

    Fix isOpApplyDg to accept non-ref args.

--- source/vibe/internal/meta/traits.d
+++ source/vibe/internal/meta/traits.d
@@ -360,13 +360,20 @@ template isOpApplyDg(DG, TKEY, TVALUE) {
 		private alias STC = ParameterStorageClass;
 		// Just a value
 		static if (PTT.length == 1) {
-			enum isOpApplyDg = (is(PTT[0] == TVALUE) && PSCT[0] == STC.ref_);
+			enum isOpApplyDg = (is(PTT[0] == TVALUE));
 		} else static if (PTT.length == 2) {
-			enum isOpApplyDg = (is(PTT[0] == TKEY) && PSCT[0] == STC.ref_)
-				&& (is(PTT[1] == TKEY) && PSCT[1] == STC.ref_);
+			enum isOpApplyDg = (is(PTT[0] == TKEY))
+				&& (is(PTT[1] == TVALUE));
 		} else
 			enum isOpApplyDg = false;
 	} else {
 		enum isOpApplyDg = false;
 	}
+}
+
+unittest {
+	static assert(isOpApplyDg!(int delegate(int, string), int, string));
+	static assert(isOpApplyDg!(int delegate(ref int, ref string), int, string));
+	static assert(isOpApplyDg!(int delegate(int, ref string), int, string));
+	static assert(isOpApplyDg!(int delegate(ref int, string), int, string));
 }
