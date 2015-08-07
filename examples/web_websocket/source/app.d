a087d5a8e990666c563ca1c56b1b8ed48cd4003a
Darius C <darius.clark@cloudvlts.net>
2015-08-07 07:07:30 -0400

    Removed unneeded imports

--- examples/web_websocket/source/app.d
+++ examples/web_websocket/source/app.d
@@ -2,12 +2,10 @@ module app;
 
 import vibe.core.core : sleep;
 
-import std.exception : enforce;
 import vibe.core.log;
 import vibe.http.fileserver : serveStaticFiles;
 import vibe.http.router : URLRouter;
 import vibe.http.server;
-import vibe.utils.validation;
 import vibe.web.web;
 import vibe.http.websockets : WebSocket, handleWebSockets;
 
