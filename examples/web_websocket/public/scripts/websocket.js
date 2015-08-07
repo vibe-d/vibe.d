c0e583da2de480fdb831df3c855dd75a902708f7
Darius C <darius.clark@cloudvlts.net>
2015-08-07 07:04:53 -0400

    Example for using the WebSocket feature with WebInterface

---
+++ examples/web_websocket/public/scripts/websocket.js
@@ -0,0 +1,37 @@
+﻿var socket
+
+function connect()
+{
+	setText("connecting...");
+	socket = new WebSocket(getBaseURL() + "/ws");
+	socket.onopen = function() {
+		setText("connected. waiting for timer...");
+	}
+	socket.onmessage = function(message) {	
+		setText(message.data);
+	}
+	socket.onclose = function() {
+		setText("connection closed.");
+	}
+	socket.onerror = function() {
+		setText("Error!");
+	}
+}
+
+function closeConnection()
+{
+	socket.close();
+	setText("closed.");
+}
+
+function setText(text)
+{
+	document.getElementById("timer").innerHTML = text;
+}
+
+function getBaseURL()
+{
+	var href = window.location.href.substring(7); // strip "http://"
+	var idx = href.indexOf("/");
+	return "ws://" + href.substring(0, idx);
+}
