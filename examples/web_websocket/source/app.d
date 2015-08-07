ee54323c773d750f0e7a0ba722d33f03d805e4f3
Darius C <darius.clark@cloudvlts.net>
2015-08-07 07:09:48 -0400

    Removed unneeded line

--- examples/web_websocket/source/app.d
+++ examples/web_websocket/source/app.d
@@ -47,7 +47,6 @@ shared static this()
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	settings.sessionStore = new MemorySessionStore;
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
