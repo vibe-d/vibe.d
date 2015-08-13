aad613913da23c72c965cfd150432e554e0ff1c2
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-08-13 22:12:50 +0200

    Fix test for #1220.

--- source/vibe/inet/webform.d
+++ source/vibe/inet/webform.d
@@ -170,8 +170,7 @@ unittest
 	assert(files["files"].filename == "file1.txt");
 }
 
-unittest
-{
+unittest { // issue #1220 - wrong handling of Content-Length
 	import vibe.stream.memory;
 
 	auto content_type = "multipart/form-data; boundary=\"AaB03x\"";
@@ -179,11 +178,16 @@ unittest
 	auto input = new MemoryStream(cast(ubyte[])
 			"--AaB03x\r\n"
 			"Content-Disposition: form-data; name=\"submit-name\"\r\n"
-			"Content-Length: 5"
 			"\r\n"
 			"Larry\r\n"
 			"--AaB03x\r\n"
 			"Content-Disposition: form-data; name=\"files\"; filename=\"file1.txt\"\r\n"
+			"Content-Type: text/plain\r\n"
+			"Content-Length: 29\r\n"
+			"\r\n"
+			"... contents of file1.txt ...\r\n"
+			"--AaB03x--\r\n"
+			"Content-Disposition: form-data; name=\"files\"; filename=\"file2.txt\"\r\n"
 			"Content-Type: text/plain\r\n"
 			"\r\n"
 			"... contents of file1.txt ...\r\n"
