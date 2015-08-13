dd319040c7eced835ae6a62d61cb7e4a2b33c429
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-08-13 21:08:23 +0200

    Add test for #1220. See also #1221.

--- source/vibe/inet/webform.d
+++ source/vibe/inet/webform.d
@@ -170,6 +170,34 @@ unittest
 	assert(files["files"].filename == "file1.txt");
 }
 
+unittest
+{
+	import vibe.stream.memory;
+
+	auto content_type = "multipart/form-data; boundary=\"AaB03x\"";
+
+	auto input = new MemoryStream(cast(ubyte[])
+			"--AaB03x\r\n"
+			"Content-Disposition: form-data; name=\"submit-name\"\r\n"
+			"Content-Length: 5"
+			"\r\n"
+			"Larry\r\n"
+			"--AaB03x\r\n"
+			"Content-Disposition: form-data; name=\"files\"; filename=\"file1.txt\"\r\n"
+			"Content-Type: text/plain\r\n"
+			"\r\n"
+			"... contents of file1.txt ...\r\n"
+			"--AaB03x--\r\n".dup, false);
+
+	FormFields fields;
+	FilePartFormFields files;
+
+	parseMultiPartForm(fields, files, content_type, input, 4096);
+
+	assert(fields["submit-name"] == "Larry");
+	assert(files["files"].filename == "file1.txt");
+}
+
 /**
 	Single part of a multipart form.
 
