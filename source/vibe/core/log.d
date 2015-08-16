4d480ca18f891048bf6e5454c2d30d4a813ef465
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-08-16 13:27:45 +0200

    Add log level documentation. Closes #1194.
    Applies the review comments in #1194.

--- source/vibe/core/log.d
+++ source/vibe/core/log.d
@@ -37,6 +37,10 @@ nothrow @safe {
 	Sets the log format used for the default console logger.
 
 	This level applies to the default stdout/stderr logger only.
+
+	Params:
+		fmt = The log format for the stderr (default is `FileLogger.Format.thread`)
+		infoFmt = The log format for the stdout (default is `FileLogger.Format.plain`)
 */
 void setLogFormat(FileLogger.Format fmt, FileLogger.Format infoFmt = FileLogger.Format.plain)
 nothrow @safe {
@@ -201,10 +205,11 @@ class Logger {
 	Plain-text based logger for logging to regular files or stdout/stderr
 */
 final class FileLogger : Logger {
+	/// The log format used by the FileLogger
 	enum Format {
-		plain,
-		thread,
-		threadTime
+		plain,      /// Output only the plain log message
+		thread,     /// Prefix "[thread-id:fiber-id loglevel]"
+		threadTime  /// Prefix "[thread-id:fiber-id timestamp loglevel]"
 	}
 
 	private {
