279316ae689b4abf2ef3a40095982c75c8b4af0b
Martin Nowak <code@dawg.eu>
2015-09-11 19:17:38 +0200

    fix BUILD_EXAMPLE and provide defaults

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,13 +5,13 @@ set -e -o pipefail
 dub build -b release --compiler=$DC --config=${VIBED_DRIVER=libevent}
 dub test --compiler=$DC --config=${VIBED_DRIVER=libevent}
 
-if [ ${RUN_EXAMPLE} -eq 1 ]; then
+if [ ${BUILD_EXAMPLE=1} -eq 1 ]; then
     for ex in $(\ls -1 examples/); do
         echo "[INFO] Building example $ex"
         (cd examples/$ex && dub build --compiler=$DC && dub clean)
     done
 fi
-if [ ${RUN_TEST} -eq 1 ]; then
+if [ ${RUN_TEST=1} -eq 1 ]; then
     for ex in `\ls -1 tests/`; do
         echo "[INFO] Running test $ex"
         (cd tests/$ex && dub --compiler=$DC && dub clean)
