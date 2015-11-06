d4555b8e5715baddc6dd89dbc9c3feb236b3f4b3
rjmcguire <rjmcguire@gmail.com>
2015-11-06 16:52:09 +0200

    Update json.d
    Can't output nan in JSON documents. Browsers don't support it. {"float":nan} is not valid JSON for example.

--- source/vibe/data/json.d
+++ source/vibe/data/json.d
@@ -1673,7 +1673,7 @@ struct JsonStringSerializer(R, bool pretty = false)
 			else static if (is(T == bool)) m_range.put(value ? "true" : "false");
 			else static if (is(T : long)) m_range.formattedWrite("%s", value);
 			else static if (is(T == BigInt)) m_range.formattedWrite("%d", value);
-			else static if (is(T : real)) m_range.formattedWrite("%.16g", value);
+			else static if (is(T : real)) m_range.formattedWrite("%.16g", value != value ? 0 : value); // output nan as 0
 			else static if (is(T == string)) {
 				m_range.put('"');
 				m_range.jsonEscape(value);
