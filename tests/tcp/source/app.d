c5f9c73c8ae368f2c31cc5212d896402083a0e59
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-11-11 11:00:48 +0100

    Add test for TCPConnection.waitForData. See #1326.

---
+++ tests/tcp/source/app.d
@@ -0,0 +1,103 @@
+import vibe.core.core;
+import vibe.core.log;
+import vibe.core.net;
+import vibe.stream.operations;
+import core.time;
+import std.datetime : StopWatch;
+
+enum port = 12675;
+
+enum Test {
+  receive,
+  timeout,
+  noTimeout,
+  noTimeoutCompat,
+  close
+}
+
+void test()
+{
+	scope (failure) assert(false);
+
+	Test test;
+
+	listenTCP(port, (conn) {
+		while (conn.connected) {
+			assert(conn.readLine() == "next");
+			conn.write("continue\r\n");
+			logInfo("Perform test %s", test);
+			StopWatch sw;
+			sw.start();
+			final switch (test) {
+				case Test.receive:
+					assert(conn.waitForData(2.seconds) == true);
+					assert(cast(Duration)sw.peek < 2.seconds); // should receive something instantly
+					assert(conn.readLine() == "receive");
+					break;
+				case Test.timeout:
+					assert(conn.waitForData(2.seconds) == false);
+					assert(cast(Duration)sw.peek >= 2.seconds); // should wait for at least 2 seconds
+					assert(conn.connected);
+					break;
+				case Test.noTimeout:
+					assert(conn.waitForData(Duration.max) == true);
+					assert(cast(Duration)sw.peek > 2.seconds); // data only sent after 3 seconds
+					assert(conn.readLine() == "noTimeout");
+					break;
+				case Test.noTimeoutCompat:
+					assert(conn.waitForData(0.seconds) == true);
+					assert(cast(Duration)sw.peek > 2.seconds); // data only sent after 3 seconds
+					assert(conn.readLine() == "noTimeoutCompat");
+					break;
+				case Test.close:
+					assert(conn.waitForData(2.seconds) == false);
+					assert(cast(Duration)sw.peek < 2.seconds); // connection should be closed instantly
+					assert(!conn.connected);
+					break;
+			}
+			conn.write("ok\r\n");
+		}
+	}, "127.0.0.1");
+
+	auto conn = connectTCP("127.0.0.1", port);
+
+	test = Test.receive;
+	conn.write("next\r\n");
+	assert(conn.readLine() == "continue");
+	conn.write("receive\r\n");
+	assert(conn.readLine() == "ok");
+
+	test = Test.timeout;
+	conn.write("next\r\n");
+	assert(conn.readLine() == "continue");
+	sleep(3.seconds);
+	assert(conn.readLine() == "ok");
+
+	test = Test.noTimeout;
+	conn.write("next\r\n");
+	assert(conn.readLine() == "continue");
+	sleep(3.seconds);
+	conn.write("noTimeout\r\n");
+	assert(conn.readLine() == "ok");
+
+	test = Test.noTimeoutCompat;
+	conn.write("next\r\n");
+	assert(conn.readLine() == "continue");
+	sleep(3.seconds);
+	conn.write("noTimeoutCompat\r\n");
+	assert(conn.readLine() == "ok");
+
+	test = Test.close;
+	conn.write("next\r\n");
+	assert(conn.readLine() == "continue");
+	conn.close();
+
+	exitEventLoop();
+}
+
+void main()
+{
+	import std.functional : toDelegate;
+	runTask(toDelegate(&test));
+	runEventLoop();
+}
