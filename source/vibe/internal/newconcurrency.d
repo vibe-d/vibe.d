36baef16b9eac2238912b8a0a06dabc166400324
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-12-02 22:11:24 +0100

    (Re-)enable std.concurrency integration on >=2.067.

--- source/vibe/internal/newconcurrency.d
+++ source/vibe/internal/newconcurrency.d
@@ -1,8 +1,4 @@
 /// Small helper module to determine if the new std.concurrency interop features are present
 module vibe.internal.newconcurrency;
 
-static if (__VERSION__ >= 2066 && false) {
-	import std.concurrency;
-	static if (is(std.concurrency.Scheduler)) enum bool newStdConcurrency = true;
-	else enum bool newStdConcurrency = false;
-} else enum bool newStdConcurrency = false;
+enum bool newStdConcurrency = __VERSION__ >= 2067;
