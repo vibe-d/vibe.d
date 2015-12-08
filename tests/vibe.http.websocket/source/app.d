98445905ff248af47c086869495b34956069a60b
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-12-08 13:19:35 +0100

    Let the websocket test use the scoped version of connectWebSocket.

--- tests/vibe.http.websocket/source/app.d
+++ tests/vibe.http.websocket/source/app.d
@@ -9,26 +9,23 @@ shared static this()
 	// 10k + issue number -> Avoid bind errors
 	settings.port = 11332;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, handleWebSockets(&onWS));
+	listenHTTP(settings, handleWebSockets((scope ws) {
+		assert(ws.receiveText() == "foo");
+		ws.send("hello");
+		assert(ws.receiveText() == "bar");
+		ws.close();
+	}));
 
 	runTask({
 		scope(exit) exitEventLoop(true);
 
-		auto ws = connectWebSocket(URL("http://127.0.0.1:11332/"));
-		ws.send("foo");
-		assert(ws.receiveText() == "hello");
-		ws.send("bar");
-		assert(!ws.waitForData);
-		ws.close();
-		logInfo("WebSocket test successful");
-        });
+		connectWebSocket(URL("http://127.0.0.1:11332/"), (scope ws) {
+			ws.send("foo");
+			assert(ws.receiveText() == "hello");
+			ws.send("bar");
+			assert(!ws.waitForData);
+			ws.close();
+			logInfo("WebSocket test successful");
+		});
+    });
 }
-
-void onWS(scope WebSocket ws)
-{
-	assert(ws.receiveText() == "foo");
-	ws.send("hello");
-	assert(ws.receiveText() == "bar");
-	ws.close();
-}
-
