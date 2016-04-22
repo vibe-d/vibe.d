408c5e9ce3994a75c4c9bc741c7c7f0dd10be11b
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-04-22 09:56:35 +0200

    Ignore UTF-8 BOM when reading vibe.conf. Fixes #1470.

--- source/vibe/core/args.d
+++ source/vibe/core/args.d
@@ -194,6 +194,8 @@ private string[] getConfigPaths()
 // this is invoked by the first readOption call (at least vibe.core will perform one)
 private void init()
 {
+	import vibe.utils.string : stripUTF8Bom;
+
 	version (VibeDisableCommandLineParsing) {}
 	else g_args = Runtime.args;
 
@@ -205,7 +207,7 @@ private void init()
 		auto cpath = buildPath(spath, configName);
 		if (cpath.exists) {
 			scope(failure) logError("Failed to parse config file %s.", cpath);
-			auto text = cpath.readText();
+			auto text = stripUTF8Bom(cpath.readText());
 			g_config = text.parseJson();
 			g_haveConfig = true;
 			break;
