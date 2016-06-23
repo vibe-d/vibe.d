87062328e73c4bbabb9550255730258152551523
David Monagle <david.monagle@intrica.com.au>
2016-06-23 16:42:25 +1000

    Removal of data opDispatch methods from mongo collection

--- source/vibe/db/mongo/collection.d
+++ source/vibe/db/mongo/collection.d
@@ -276,12 +276,12 @@ struct MongoCollection {
 		cmd.count = m_name;
 		cmd.query = query;
 		auto reply = database.runCommand(cmd);
-		enforce(reply.ok.opt!double == 1 || reply.ok.opt!int == 1, "Count command failed.");
-		switch (reply.n.type) with (Bson.Type) {
+		enforce(reply["ok"].opt!double == 1 || reply["ok"].opt!int == 1, "Count command failed.");
+		switch (reply["n"].type) with (Bson.Type) {
 			default: assert(false, "Unsupported data type in BSON reply for COUNT");
-			case double_: return cast(ulong)reply.n.get!double; // v2.x
-			case int_: return reply.n.get!int; // v3.x
-			case long_: return reply.n.get!long; // just in case
+			case double_: return cast(ulong)reply["n"].get!double; // v2.x
+			case int_: return reply["n"].get!int; // v3.x
+			case long_: return reply["n"].get!long; // just in case
 		}
 	}
 
