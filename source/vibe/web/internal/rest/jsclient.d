db44eaf377ab5b24d6d63df9dae41d44d4886ffb
deviator <code.viator@gmail.com>
2016-09-19 01:11:42 +0300

    fix rest js client formating

--- source/vibe/web/internal/rest/jsclient.d
+++ source/vibe/web/internal/rest/jsclient.d
@@ -15,7 +15,8 @@ import std.conv : to;
 /**
 	Generates JavaScript code suitable for accessing a REST interface using XHR.
 */
-/*package(vibe.web.web)*/ void generateInterface(TImpl, R)(ref R output, string name, RestInterfaceSettings settings, bool child=false)
+/*package(vibe.web.web)*/ void generateInterface(TImpl, R)(ref R output, string name, RestInterfaceSettings settings,
+		string baseIndent="", string indentStep = "    ", bool child=false)
 {
 	// TODO: handle attributed parameters and filter out internal parameters that have no path placeholder assigned to them
 
@@ -30,12 +31,18 @@ import std.conv : to;
 
 	auto intf = RestInterface!TImpl(settings, true);
 
-	output.formattedWrite("%s%s = new function() {\n", child ? "this." : "", name.length ? name : intf.I.stringof);
+	output.formattedWrite("%s%s%s = new function() {\n", baseIndent, child ? "this." : "", name.length ? name : intf.I.stringof);
 
-	output.put("var toRestString = function(v) { return v; }\n");
+	auto indent = baseIndent ~ indentStep;
+	auto inner1 = indent ~ indentStep;
+	auto inner2 = inner1 ~ indentStep;
+
+	output.put(indent ~ "var toRestString = function(v) { return v; }\n");
 
 	foreach (i, SI; intf.SubInterfaceTypes) {
-		output.generateInterface!SI(__traits(identifier, intf.SubInterfaceFunctions[i]), intf.subInterfaces[i].settings, true);
+		output.put("\n");
+		output.generateInterface!SI(__traits(identifier, intf.SubInterfaceFunctions[i]),
+				intf.subInterfaces[i].settings, indent, indentStep, true);
 	}
 
 	foreach (i, F; intf.RouteFunctions) {
@@ -43,43 +50,43 @@ import std.conv : to;
 		auto route = intf.routes[i];
 
 		// function signature
-		output.formattedWrite("  this.%s = function(", route.functionName);
+		output.put("\n");
+		output.formattedWrite("%sthis.%s = function(", indent, route.functionName);
 		foreach (j, param; route.parameters) {
 			output.put(param.name);
 			output.put(", ");
 		}
 		static if (!is(ReturnType!FT == void)) output.put("on_result, ");
+
 		output.put("on_error) {\n");
 
 		// url assembly
+		output.put(inner1 ~ "var url = ");
 		if (route.pathHasPlaceholders) {
-			output.put("    var url = ");
 			output.serializeToJson(intf.baseURL);
 			foreach (p; route.pathParts) {
 				output.put(" + ");
 				if (!p.isParameter) output.serializeToJson(p.text);
 				else output.formattedWrite("encodeURIComponent(toRestString(%s))", p.text);
 			}
-			output.put(";\n");
 		} else {
-			import std.traits;
-			string[] pit = [ParameterIdentifierTuple!F];
-			if (pit.length == 0)
-				output.formattedWrite("    var url = %s;\n", Json(concatURL(intf.baseURL, route.pattern)));
-			else
-			{
-				import std.algorithm;
+			import std.algorithm;
+			auto rpn = route.parameters.map!"a.name".filter!(a=>a[0]=='_').map!"a[1..$]";
+			if (rpn.save.count == 0)
+				output.formattedWrite("%s", Json(concatURL(intf.baseURL, route.pattern.dup)));
+			else {
 				import std.array;
 				char[] sink = route.pattern.dup;
-				foreach (param; pit.filter!(a=>a[0]=='_').map!(a=>a[1..$]))
+				foreach (param; rpn)
 					sink = replace(sink, ":" ~ param, "${_" ~ param ~ "}");
-				output.formattedWrite("    var url = `%s`;\n", intf.baseURL ~ sink.idup); // use `` instead ""
+				output.formattedWrite("`%s`", concatURL(intf.baseURL, sink.idup)); // use `` instead ""
 			}
 		}
+		output.put(";\n");
 
 		// query parameters
 		if (route.queryParameters.length) {
-			output.put("    url = url");
+			output.put(inner1 ~ "url = url");
 			foreach (j, p; route.queryParameters)
 				output.formattedWrite(" + \"%s%s=\" + encodeURIComponent(toRestString(%s))",
 					j == 0 ? '?' : '&', p.fieldName, p.name);
@@ -88,35 +95,38 @@ import std.conv : to;
 
 		// body parameters
 		if (route.bodyParameters.length) {
-			output.put("    var postbody = {\n");
+			output.put(inner1 ~ "var postbody = {\n");
 			foreach (p; route.bodyParameters)
-				output.formattedWrite("      %s: toRestString(%s),\n", Json(p.fieldName), p.name);
-			output.put("    };\n");
+				output.formattedWrite("%s%s: toRestString(%s),\n", inner2, Json(p.fieldName), p.name);
+			output.put(inner1 ~ "};\n");
 		}
 
 		// XHR setup
-		output.put("    var xhr = new XMLHttpRequest();\n");
-		output.formattedWrite("    xhr.open('%s', url, true);\n", route.method.to!string.toUpper);
+		output.put(inner1 ~ "var xhr = new XMLHttpRequest();\n");
+		output.formattedWrite("%sxhr.open('%s', url, true);\n", inner1, route.method.to!string.toUpper);
 		static if (!is(ReturnType!FT == void)) {
-			output.put("    xhr.onload = function () { if (this.status >= 400) { if (on_error) on_error(JSON.parse(this.responseText)); else console.log(this.responseText); } else on_result(JSON.parse(this.responseText)); };\n");
+			output.put(inner1 ~ "xhr.onload = function () {\n");
+			output.put(inner2 ~ "if (this.status >= 400) { if (on_error) on_error(JSON.parse(this.responseText)); else console.log(this.responseText); }\n");
+			output.put(inner2 ~ "else on_result(JSON.parse(this.responseText));\n");
+			output.put(inner1 ~ "};\n");
 		}
 
 		// header parameters
 		foreach (p; route.headerParameters)
-			output.formattedWrite("    xhr.setRequestHeader(%s, %s);\n", Json(p.fieldName), p.name);
+			output.formattedWrite("%sxhr.setRequestHeader(%s, %s);\n", inner1, Json(p.fieldName), p.name);
 
 		// submit request
 		if (route.method == HTTPMethod.GET || !route.bodyParameters.length)
-			output.put("    xhr.send();\n");
+			output.put(inner1 ~ "xhr.send();\n");
 		else {
-			output.put("    xhr.setRequestHeader('Content-Type', 'application/json');\n");
-			output.put("    xhr.send(JSON.stringify(postbody));\n");
+			output.put(inner1 ~ "xhr.setRequestHeader('Content-Type', 'application/json');\n");
+			output.put(inner1 ~ "xhr.send(JSON.stringify(postbody));\n");
 		}
 
-		output.put("  }\n\n");
+		output.put(indent ~ "}\n");
 	}
 
-	output.put("}\n");
+	output.put(baseIndent ~ "}\n");
 }
 
 version (unittest) {
