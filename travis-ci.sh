7976d6d2726d3213340214b4f5cb1f63f173c1c7
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-01-27 01:36:20 +0100

    Fix the LDC part of the CI test script.

--- travis-ci.sh
+++ travis-ci.sh
@@ -11,13 +11,13 @@ if [ "$DC" == "dmd" ]; then
 fi
 
 if [ "$DC" == "ldc2" ] && [ "$VIBED_DRIVER" == "vibe-core" ]; then
-	dub test :data --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
+	dub test :data --compiler=$DC
 	dub test :core --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :mongodb --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :redis --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :diet --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :web --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
-	dub test :utils --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
+	dub test :utils --compiler=$DC
 	dub test :http --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :mail --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
 	dub test :stream --compiler=$DC --override-config=vibe-d:core/${VIBED_DRIVER=libevent}
