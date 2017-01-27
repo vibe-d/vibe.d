836451e88e62e37c0d8c0e6385dd3311085111f8
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-01-27 01:24:09 +0100

    Adjust echo server example for vibe-core.

--- examples/echoserver/source/app.d
+++ examples/echoserver/source/app.d
@@ -1,7 +1,11 @@
 import vibe.appmain;
 import vibe.core.net;
+import vibe.core.stream;
 
 shared static this()
 {
-	listenTCP(2000, conn => conn.write(conn));
+	listenTCP(2000, (conn) {
+		try conn.pipe(conn);
+		catch (Exception e) conn.close();
+	});
 }
