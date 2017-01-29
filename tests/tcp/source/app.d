414fda16e379a247e3f50319ebe472d27140520c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-01-30 00:50:46 +0100

    Fix TCP test.
    - Remove legacy test case (wait timeout 0 meaning infinity)
    - Make the core compile with vibe-core
    - Fix the use of connected vs. empty properties

--- tests/tcp/source/app.d
+++ tests/tcp/source/app.d
@@ -12,7 +12,6 @@ enum Test {
   receiveExisting,
   timeout,
   noTimeout,
-  noTimeoutCompat,
   close
 }
 
@@ -25,48 +24,48 @@ void test1()
 
 	listenTCP(port, (conn) {
 		lt = Task.getThis();
-		while (conn.connected) {
-			assert(conn.readLine() == "next");
-			auto curtest = test;
-			conn.write("continue\r\n");
-			logInfo("Perform test %s", curtest);
-			StopWatch sw;
-			sw.start();
-			final switch (curtest) {
-				case Test.receive:
-					assert(conn.waitForData(2.seconds) == true);
-					assert(cast(Duration)sw.peek < 2.seconds); // should receive something instantly
-					assert(conn.readLine() == "receive");
-					break;
-				case Test.receiveExisting:
-					assert(conn.waitForData(2.seconds) == true);
-					// TODO: validate that waitForData didn't yield!
-					assert(cast(Duration)sw.peek < 2.seconds); // should receive something instantly
-					assert(conn.readLine() == "receiveExisting");
-					break;
-				case Test.timeout:
-					assert(conn.waitForData(2.seconds) == false);
-					assert(cast(Duration)sw.peek > 1900.msecs); // should wait for at least 2 seconds
-					assert(conn.connected);
-					break;
-				case Test.noTimeout:
-					assert(conn.waitForData(Duration.max) == true);
-					assert(cast(Duration)sw.peek > 2.seconds); // data only sent after 3 seconds
-					assert(conn.readLine() == "noTimeout");
-					break;
-				case Test.noTimeoutCompat:
-					assert(conn.waitForData(0.seconds) == true);
-					assert(cast(Duration)sw.peek > 2.seconds); // data only sent after 3 seconds
-					assert(conn.readLine() == "noTimeoutCompat");
-					break;
-				case Test.close:
-					assert(conn.waitForData(2.seconds) == false);
-					assert(cast(Duration)sw.peek < 2.seconds); // connection should be closed instantly
-					assert(!conn.connected);
-					conn.close();
-					return;
+		try {
+			while (!conn.empty) {
+				assert(conn.readLine() == "next");
+				auto curtest = test;
+				conn.write("continue\r\n");
+				logInfo("Perform test %s", curtest);
+				StopWatch sw;
+				sw.start();
+				final switch (curtest) {
+					case Test.receive:
+						assert(conn.waitForData(2.seconds) == true);
+						assert(cast(Duration)sw.peek < 2.seconds); // should receive something instantly
+						assert(conn.readLine() == "receive");
+						break;
+					case Test.receiveExisting:
+						assert(conn.waitForData(2.seconds) == true);
+						// TODO: validate that waitForData didn't yield!
+						assert(cast(Duration)sw.peek < 2.seconds); // should receive something instantly
+						assert(conn.readLine() == "receiveExisting");
+						break;
+					case Test.timeout:
+						assert(conn.waitForData(2.seconds) == false);
+						assert(cast(Duration)sw.peek > 1900.msecs); // should wait for at least 2 seconds
+						assert(conn.connected);
+						break;
+					case Test.noTimeout:
+						assert(conn.waitForData(Duration.max) == true);
+						assert(cast(Duration)sw.peek > 2.seconds); // data only sent after 3 seconds
+						assert(conn.readLine() == "noTimeout");
+						break;
+					case Test.close:
+						assert(conn.waitForData(2.seconds) == false);
+						assert(cast(Duration)sw.peek < 2.seconds); // connection should be closed instantly
+						assert(conn.empty);
+						conn.close();
+						assert(!conn.connected);
+						return;
+				}
+				conn.write("ok\r\n");
 			}
-			conn.write("ok\r\n");
+		} catch (Exception e) {
+			assert(false, e.msg);
 		}
 	}, "127.0.0.1");
 
@@ -96,13 +95,6 @@ void test1()
 	conn.write("noTimeout\r\n");
 	assert(conn.readLine() == "ok");
 
-	test = Test.noTimeoutCompat;
-	conn.write("next\r\n");
-	assert(conn.readLine() == "continue");
-	sleep(3.seconds);
-	conn.write("noTimeoutCompat\r\n");
-	assert(conn.readLine() == "ok");
-
 	test = Test.close;
 	conn.write("next\r\n");
 	assert(conn.readLine() == "continue");
@@ -116,18 +108,22 @@ void test2()
 	Task lt;
 	logInfo("Perform test \"disconnect with pending data\"");
 	listenTCP(port+1, (conn) {
-		lt = Task.getThis();
-		sleep(1.seconds);
-		StopWatch sw;
-		sw.start();
 		try {
-			assert(conn.waitForData() == true);
-			assert(cast(Duration)sw.peek < 500.msecs); // waitForData should return immediately
-			assert(conn.dataAvailableForRead);
-			assert(conn.readAll() == "test");
-			conn.close();
+			lt = Task.getThis();
+			sleep(1.seconds);
+			StopWatch sw;
+			sw.start();
+			try {
+				assert(conn.waitForData() == true);
+				assert(cast(Duration)sw.peek < 500.msecs); // waitForData should return immediately
+				assert(conn.dataAvailableForRead);
+				assert(conn.readAll() == "test");
+				conn.close();
+			} catch (Exception e) {
+				assert(false, "Failed to read pending data: " ~ e.msg);
+			}
 		} catch (Exception e) {
-			assert(false, "Failed to read pending data: " ~ e.msg);
+			assert(false, e.msg);
 		}
 	}, "127.0.0.1");
 
