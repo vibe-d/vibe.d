84a5ceb647d08dcde8baf8ac24a77629d5f37e34
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-01-30 00:13:17 +0100

    Fix shutdown procedure in RedisSubscriber.blisten().
    (cherry picked from commit ecdf5150b3b541787e183a6d0de7c06dc3fec626)

--- source/vibe/db/redis/redis.d
+++ source/vibe/db/redis/redis.d
@@ -918,7 +918,7 @@ final class RedisSubscriberImpl {
 	private void init(){
 
 		logTrace("init");
-		if (m_lockedConnection.__conn is null){
+		if (!m_lockedConnection) {
 			m_lockedConnection = m_client.m_connections.lockConnection();
 			m_lockedConnection.setAuth(m_client.m_authPassword);
 			m_lockedConnection.setDB(m_client.m_selectedDB);
@@ -1100,6 +1100,7 @@ final class RedisSubscriberImpl {
 			if (!m_stop) {
 				stop(); // notifies the listenerHelper
 			}
+			m_listenerHelper.join();
 			// close the data connections
 			teardown();
 
