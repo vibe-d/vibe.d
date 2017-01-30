61cb31e76c627a07fb9e661db4d846017e9dcc91
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-01-30 16:57:35 +0100

    Fix HTTP Host header test.
    The TCPConnection.connected property was used incorrectly.

--- tests/vibe.http.server.host-header/source/app.d
+++ tests/vibe.http.server.host-header/source/app.d
@@ -15,38 +15,44 @@ shared static this()
 	listenHTTP(s1, &handler);
 
 	runTask({
-		scope (failure) assert(false);
+		try {
+			auto conn = connectTCP("::1", 11388);
+			conn.write("GET / HTTP/1.1\r\nHost: [::1]\r\n\r\n");
+			string res = cast(string)conn.readLine();
+			assert(res == "HTTP/1.1 200 OK", res);
+			while (conn.readLine().length > 0) {}
+			assert(cast(string)conn.readLine() == "success");
+			logInfo("1.1 with Host header OK.");
 
-		auto conn = connectTCP("::1", 11388);
-		conn.write("GET / HTTP/1.1\r\nHost: [::1]\r\n\r\n");
-		string res = cast(string)conn.readLine();
-		assert(res == "HTTP/1.1 200 OK", res);
-		while (conn.readLine().length > 0) {}
-		assert(cast(string)conn.readLine() == "success");
-		logInfo("1.1 with Host header OK.");
+			conn.write("GET / HTTP/1.1\r\n\r\n");
+			res = cast(string)conn.readLine();
+			assert(res == "HTTP/1.1 400 Bad Request", res);
+			while (conn.readLine().length > 0) {}
+			ubyte[39] buf;
+			conn.read(buf);
+			assert(cast(string)buf == "400 - Bad Request\n\nMissing Host header.");
+			assert(!conn.waitForData(1.seconds));
+			assert(conn.empty);
+			conn.close();
+			assert(!conn.connected);
+			logInfo("1.1 without Host header OK.");
 
-		conn.write("GET / HTTP/1.1\r\n\r\n");
-		res = cast(string)conn.readLine();
-		assert(res == "HTTP/1.1 400 Bad Request", res);
-		while (conn.readLine().length > 0) {}
-		ubyte[39] buf;
-		conn.read(buf);
-		assert(cast(string)buf == "400 - Bad Request\n\nMissing Host header.");
-		conn.waitForData(1.seconds);
-		assert(!conn.connected && conn.empty);
-		logInfo("1.1 without Host header OK.");
-
-		conn = connectTCP("::1", 11388);
-		conn.write("GET / HTTP/1.0\r\n\r\n");
-		res = cast(string)conn.readLine();
-		assert(res == "HTTP/1.0 200 OK", res);
-		while (conn.readLine().length > 0) {}
-		assert(cast(string)conn.readLine() == "success");
-		conn.waitForData(1.seconds);
-		assert(!conn.connected && conn.empty);
-		conn.waitForData(1.seconds);
-		assert(!conn.connected && conn.empty);
-		logInfo("1.0 without Host header OK.");
+			conn = connectTCP("::1", 11388);
+			conn.write("GET / HTTP/1.0\r\n\r\n");
+			res = cast(string)conn.readLine();
+			assert(res == "HTTP/1.0 200 OK", res);
+			while (conn.readLine().length > 0) {}
+			assert(cast(string)conn.readLine() == "success");
+			assert(!conn.waitForData(1.seconds));
+			assert(conn.empty);
+			conn.close();
+			assert(!conn.connected);
+			assert(!conn.waitForData(1.seconds));
+			assert(!conn.connected && conn.empty);
+			logInfo("1.0 without Host header OK.");
+		} catch (Exception e) {
+			assert(false, e.msg);
+		}
 
 		scope (exit) exitEventLoop();
 	});
