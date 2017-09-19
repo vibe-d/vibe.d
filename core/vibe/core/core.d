2f5fff66e5b5016230e2ac10bc3eea0907c1078a
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-09-19 23:17:49 +0200

    bump version to v0.8.2-alpha.1

--- core/vibe/core/core.d
+++ core/vibe/core/core.d
@@ -987,7 +987,7 @@ void setTaskEventCallback(TaskEventCb func)
 /**
 	A version string representing the current vibe.d version
 */
-enum vibeVersionString = "0.8.1";
+enum vibeVersionString = "0.8.2-alpha.1";
 
 
 /**
