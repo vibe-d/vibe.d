bd33221e35e93799db63fadd5081356bcdd1cb3f
Geoffrey <geoffrey3a@gmail.com>
2017-12-21 10:42:18 +1300

    Fix RedisHash.opIndexOpAssign. The snippet `m_db.getAsHash!int("bank_accounts")["bob"] += 100;` failed with the following error: ../../.dub/packages/vibe-d-0.8.2/vibe-d/redis/vibe/db/redis/types.d(392,84): Error: none of the overloads of 'hincr' are callable using argument types (string, string, string), candidates are: ../../.dub/packages/vibe-d-0.8.2/vibe-d/redis/vibe/db/redis/redis.d(318,7):        vibe.db.redis.redis.RedisDatabase.hincr(string key, string field, long value = 1L) ../../.dub/packages/vibe-d-0.8.2/vibe-d/redis/vibe/db/redis/redis.d(320,7):        vibe.db.redis.redis.RedisDatabase.hincr(string key, string field, double value)

--- redis/vibe/db/redis/types.d
+++ redis/vibe/db/redis/types.d
@@ -389,7 +389,7 @@ struct RedisHash(T = string) {
 		return m_db.hsetNX(m_key, field, value.toRedis());
 	}
 
-	void opIndexOpAssign(string op)(T value, string field) if (op == "+") { m_db.hincr(m_key, field, value.toRedis()); }
+	void opIndexOpAssign(string op)(T value, string field) if (op == "+") { m_db.hincr(m_key, field, value); }
 
 	int opApply(scope int delegate(string key, T value) del)
 	{
