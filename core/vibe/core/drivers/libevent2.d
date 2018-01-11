f7dcd0e6c48c14f6c42855367dde6eb73463f217
Diederik de Groot <ddegroot@talon.nl>
2018-01-11 10:03:38 +0100

    Remove static compiler __VERSION check. 'import core.sys.dragonflybsd.netinet.in_' is fine, when compiler support is there.

--- core/vibe/core/drivers/libevent2.d
+++ core/vibe/core/drivers/libevent2.d
@@ -85,12 +85,7 @@ else version (FreeBSD)
 }
 else version (DragonFlyBSD)
 {
-	static if (__VERSION__ < 2077) {
-		enum IP_ADD_MEMBERSHIP  = 12;
-		enum IP_MULTICAST_LOOP  = 11;
-	}
-	else
-		import core.sys.dragonflybsd.netinet.in_ : IP_ADD_MEMBERSHIP, IP_MULTICAST_LOOP;
+    import core.sys.dragonflybsd.netinet.in_ : IP_ADD_MEMBERSHIP, IP_MULTICAST_LOOP;
 }
 else version (linux)
 {
