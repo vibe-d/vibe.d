42893373fe42e43c09f884f69fb1b66c0ae4554e
Sönke Ludwig <sludwig@outerproduct.org>
2019-02-18 14:05:49 +0100

    Add HTTPS web socket test.

--- tests/vibe.http.websocket.2169/source/app.d
+++ tests/vibe.http.websocket.2169/source/app.d
@@ -3,12 +3,19 @@ import vibe.core.log;
 import vibe.inet.url;
 import vibe.http.server;
 import vibe.http.websockets;
+import vibe.stream.tls;
 
-void main()
+
+void test(bool tls)
 {
 	auto settings = new HTTPServerSettings;
 	settings.port = 0;
 	settings.bindAddresses = ["127.0.0.1"];
+	if (tls) {
+		settings.tlsContext = createTLSContext(TLSContextKind.server);
+		settings.tlsContext.useCertificateChainFile("../tls/server.crt");
+		settings.tlsContext.usePrivateKeyFile("../tls/server.key");
+	}
 	auto listener = listenHTTP(settings, handleWebSockets((scope ws) {
 		assert(ws.connected); // issue #2104
 		assert(ws.receiveText() == "foo");
@@ -18,18 +25,20 @@ void main()
 	}));
 
 	const serverAddr = listener.bindAddresses[0];
+	const server_url = URL((tls ? "https://" : "http://") ~ serverAddr.toString);
 
 	runTask({
 		scope(exit) exitEventLoop(true);
 
 		try {
 			// issue #2169 - calling connectWebSocket twice
-			auto ws1 = connectWebSocket(URL("http://" ~ serverAddr.toString));
+			auto ws1 = connectWebSocket(server_url);
 			yield();
-			auto ws2 = connectWebSocket(URL("http://" ~ serverAddr.toString));
+			auto ws2 = connectWebSocket(server_url);
 
 			testWS(ws1);
 			testWS(ws2);
+			yield();
 		} catch (Exception e) {
 			assert(false, "Web sockets failed: "~e.msg);
 		}
@@ -38,6 +47,12 @@ void main()
     runApplication();
 
     listener.stopListening();
+}
+
+void main()
+{
+	test(false);
+	test(true);
 }
 
 
