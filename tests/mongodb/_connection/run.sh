8db268decda1da19d90d13975fec278f7b22d645
WebFreak001 <gh@webfreak.org>
2019-05-11 20:42:33 +0200

    fix mongo login tests

--- tests/mongodb/_connection/run.sh
+++ tests/mongodb/_connection/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONGOPORT=22824
-rm -f log.txt
+rm -f log.txt*
 rm -rf db
 mkdir -p db/noauth
 mkdir -p db/wcert
@@ -27,7 +27,7 @@ fi
 # Authenticated Test
 
 MONGOPID=$(mongod --logpath log.txt --bind_ip 127.0.0.1 --port $MONGOPORT --noauth --dbpath db/auth --fork | grep -Po 'forked process: \K\d+')
-echo "db.createUser({user:'admin',pwd:'123456',roles:['readWrite','dbAdmin']})" | mongo "mongodb://127.0.0.1:$MONGOPORT/admin"
+echo "db.createUser({user:'admin',pwd:'123456',roles:[{role:'readWrite',db:'unittest'},'dbAdmin'],passwordDigestor:'server'})" | mongo "mongodb://127.0.0.1:$MONGOPORT/admin"
 kill $MONGOPID
 
 while kill -0 $MONGOPID; do 
@@ -37,15 +37,20 @@ done
 MONGOPID=$(mongod --logpath log.txt --bind_ip 127.0.0.1 --port $MONGOPORT --auth --dbpath db/auth --fork | grep -Po 'forked process: \K\d+')
 sleep 1
 
+echo Trying unauthenticated operations on a protected database
 if ! eval $DUB_INVOKE -- $MONGOPORT faildb ; then
 	kill $MONGOPID
 	exit 1
 fi
-if ! eval $DUB_INVOKE -- $MONGOPORT auth admin 123456 ; then
+
+echo Trying wrongly authenticated operations on a protected database
+if ! eval $DUB_INVOKE -- $MONGOPORT failauth auth admin 1234567 ; then
 	kill $MONGOPID
 	exit 1
 fi
-if ! eval $DUB_INVOKE -- $MONGOPORT failauth auth admin 1234567 ; then
+
+echo Trying authenticated operations on a protected database
+if ! eval $DUB_INVOKE -- $MONGOPORT auth admin 123456 ; then
 	kill $MONGOPID
 	exit 1
 fi
--- tests/mongodb/_connection/source/app.d
+++ tests/mongodb/_connection/source/app.d
@@ -2,7 +2,9 @@ import vibe.db.mongo.mongo;
 import vibe.db.mongo.client;
 import vibe.core.core;
 import vibe.core.log;
+import core.time;
 import std.conv;
+import std.exception;
 
 int main(string[] args)
 {
@@ -17,7 +19,10 @@ int main(string[] args)
 		return 1;
 	}
 
+	runTask({ sleep(10.seconds); assert(false, "Timeout exceeded"); });
+
 	port = args[1].to!ushort;
+	MongoClientSettings settings = new MongoClientSettings;
 
 	int authStep = 0;
 	foreach (arg; args[2 .. $])
@@ -25,6 +30,7 @@ int main(string[] args)
 		if (authStep == 1)
 		{
 			username = arg;
+			authStep = 2;
 		}
 		else if (authStep == 2)
 		{
@@ -46,7 +52,6 @@ int main(string[] args)
 		}
 	}
 
-	MongoClientSettings settings = new MongoClientSettings;
 	settings.hosts = [MongoHost("127.0.0.1", port)];
 	settings.appName = "VibeConnectTest";
 	if (username.length)
@@ -58,33 +63,39 @@ int main(string[] args)
 
 	try
 	{
+		logInfo("Trying to connect to %(%s, %)", settings.hosts);
 		client = connectMongoDB(settings);
 	}
 	catch (MongoAuthException e)
 	{
 		if (failAuth)
+		{
+			logInfo("Got expected failure in authenticating: %s", e.msg);
 			return 0;
+		}
 		throw e;
 	}
 	catch (MongoDriverException e)
 	{
 		if (failConnect)
+		{
+			logInfo("Got expected failure in connection: %s", e.msg);
 			return 0;
+		}
 		throw e;
 	}
 
-	if (failConnect && !client)
-	{
-		throw new Exception("Expected connection failure, but none was thrown");
-	}
+	if (failConnect)
+		enforce(!client, "Expected connection failure, but none was thrown");
 
-	if (failAuth && !client)
-	{
-		throw new Exception("Expected authentication failure, but none was thrown");
-	}
+	if (failAuth)
+		enforce(!client, "Expected authentication failure, but none was thrown");
 
+	assert(!failConnect);
+	assert(!failAuth);
 	assert(client);
 
+	logInfo("Accessing collection unittest.collection");
 	auto db = client.getDatabase("unittest");
 	MongoCollection coll = db["collection"];
 
@@ -92,25 +103,47 @@ int main(string[] args)
 
 	try
 	{
-		coll.insert(Bson(["_id" : Bson(objID), "hello" : Bson("world")]));
+		logInfo(`Trying to insert {"_id": "%s", "hello": "world"}`, objID);
+		coll.insert(Bson(["_id": Bson(objID), "hello": Bson("world")]));
 	}
 	catch (MongoDriverException e)
 	{
 		if (failDB)
+		{
+			logInfo("Got expected failure in inserting: %s", e.msg);
 			return 0;
+		}
 		throw e;
 	}
 
+	// TODO: implement writeConcern so this can be tested properly!
+	// if (failDB)
+	// 	throw new Exception("Expected insertion failure, but none was thrown");
 	if (failDB)
-		throw new Exception("Expected insertion failure, but none was thrown");
+	{
+		bool exists;
+		try
+		{
+			exists = !coll.findOne(["_id": objID]).isNull;
+		}
+		catch (MongoDriverException)
+		{
+			// auth fail, is ok if we expect insert not to work until writeConcern is implemented
+		}
+		enforce(!exists, "Expected insertion failure, but item got inserted");
+		return 0;
+	}
 
 	logInfo("Everything in DB (target=%s):", objID);
 	foreach (v; coll.find())
 		logInfo("\t%s", v);
 
-	auto v = coll.findOne(["_id" : objID]);
+	auto v = coll.findOne(["_id": objID]);
+	assert(!v.isNull, "Just-inserted entry is not added to the database");
 	assert(v["hello"].get!string == "world",
 			"Mongo server didn't operate as epxected. Got " ~ v["hello"].to!string ~ " instead of world!");
+	logInfo(`just-inserted {"hello": "world"} entry found`);
 
+	logInfo("All tests passed");
 	return 0;
 }
