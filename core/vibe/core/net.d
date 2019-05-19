1a164c519810a6a5a8b871b2891a4f4035e9ee02
Sönke Ludwig <sludwig@rejectedsoftware.com>
2019-05-19 11:33:31 +0200

    Fix duplicate declaration of TCPListenOptions.defaults

--- core/vibe/core/net.d
+++ core/vibe/core/net.d
@@ -455,7 +455,7 @@ interface UDPConnection {
 */
 enum TCPListenOptions {
 	/// Don't enable any particular option
-	defaults = 0,
+	none = 0,
 	/// Causes incoming connections to be distributed across the thread pool
 	distribute = 1<<0,
 	/// Disables automatic closing of the connection when the connection callback exits
@@ -466,7 +466,7 @@ enum TCPListenOptions {
 	reusePort = 1<<2,
 	/// Enable address reuse
 	reuseAddress = 1<<3,
-	///
+	/// Enables address reuse by default
 	defaults = reuseAddress
 }
 
