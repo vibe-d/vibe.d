72a6411767da270fc298cbe468977f559d6f0a8e
Geod24 <pro.mathias.lang@gmail.com>
2020-01-29 20:29:51 +0900

    vibe.http.client.1426: Disable parallel GC

--- tests/vibe.http.client.1426/source/app.d
+++ tests/vibe.http.client.1426/source/app.d
@@ -3,6 +3,10 @@ import vibe.core.net;
 import vibe.http.client;
 import vibe.stream.operations;
 
+/// Workaround segv caused by parallel GC
+static if (__VERSION__ >= 2087)
+    extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
+
 int main ()
 {
 	immutable serverAddr = listenTCP(0, (TCPConnection c) @safe nothrow {
