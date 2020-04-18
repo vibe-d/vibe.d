3ad28f425bc0ef21da68cf644ad3924308edb566
WebFreak001 <gh@webfreak.org>
2020-04-18 21:02:03 +0200

    fix multipart test to actually run

--- tests/vibe.http.client.1005/source/app.d
+++ tests/vibe.http.client.1005/source/app.d
@@ -7,8 +7,10 @@ import vibe.http.server;
 import vibe.stream.operations;
 import vibe.stream.memory;
 
-shared static this()
+void main(string[] args)
 {
+	bool handled = false;
+
 	listenHTTP("127.0.0.1:11005", (scope req, scope res) {
 		assert(req.form.length == 1);
 		assert(req.files.length == 1);
@@ -20,19 +22,21 @@ shared static this()
 		assert(data == "Totally\0a\0PNG\0file.");
 		removeFile(req.files["picture"].tempPath);
 		res.writeBody("ok");
+		handled = true;
 	});
 
-	runTask({
-		requestHTTP("http://127.0.0.1:11005",
-			(scope req) {
-				MultiPartBody part = new MultiPartBody;
-				part.parts ~= MultiPart.formData("name", "bob");
-				auto memStream = createMemoryStream(cast(ubyte[]) "Totally\0a\0PNG\0file.", false);
-				part.parts ~= MultiPart.singleFile("picture", "picture.png", "image/png", memStream, true);
-				req.writePart(part);
-			},
-			(scope res) {}
-		);
-		exitEventLoop();
-	});
+	requestHTTP("http://127.0.0.1:11005",
+		(scope req) {
+			MultiPartBody part = new MultiPartBody;
+			part.parts ~= MultiPart.formData("name", "bob");
+			auto memStream = createMemoryStream(cast(ubyte[]) "Totally\0a\0PNG\0file.", false);
+			part.parts ~= MultiPart.singleFile("picture", "picture.png", "image/png", memStream, true);
+			req.writeMultiPartBody(part);
+		},
+		(scope res) {
+			assert(res.bodyReader.readAllUTF8() == "ok");
+		}
+	);
+
+	assert(handled);
 }
