vibe-d/vibe.d

commit 0046c48bd8d85fe68fd07b1fa94ddabaf57e2f13

Replace randomUUID by securely generated random data for digest authentication.
randomUUID does not use a secure RNG and exposes potential security issues, namely if sequences of randomly generated UUIDs (or other random data based on the same RNG) in other places of an application are exposed to an attacker, it becomes possible to predict the digest auth secret.

See https://forum.dlang.org/thread/zjmyxudvscfwrkrdabmt@forum.dlang.org

http/vibe/http/auth/digest_auth.d
@@ -9,8 +9,9 @@
 */
 module vibe.http.auth.digest_auth;
 
-import vibe.http.server;
 import vibe.core.log;
+import vibe.crypto.cryptorand;
+import vibe.http.server;
 import vibe.inet.url;
 
 import std.base64;
@@ -18,7 +19,6 @@ import std.datetime;
 import std.digest.md;
 import std.exception;
 import std.string;
-import std.uuid;
 
 @safe:
 
@@ -29,12 +29,12 @@ class DigestAuthInfo
 	@safe:
 
 	string realm;
-	ubyte[] secret;
+	ubyte[32] secret;
 	ulong timeout;
 
 	this()
 	{
-		secret = randomUUID().data.dup;
+		secureRNG.read(secret[]);
 		timeout = 300;
 	}
 

commit 3a6bbb8dd28148e8d5eeb6b2c9fe1db167c3f070

Fix secret length mismatch at digest auth
This fixes https://github.com/vibe-d/vibe.d/issues/2597.
It has been broken after https://github.com/vibe-d/vibe.d/commit/0046c48bd8d85fe68fd07b1fa94ddabaf57e2f13
 was committed.

http/vibe/http/auth/digest_auth.d
@@ -29,7 +29,7 @@ class DigestAuthInfo
 	@safe:
 
 	string realm;
-	ubyte[32] secret;
+	ubyte[16] secret;
 	ulong timeout;
 
 	this()