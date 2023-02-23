9adc6a5dab8a60f1545495067a461e3acc63fb14
Sönke Ludwig <sludwig@outerproduct.org>
2023-02-23 13:29:00 +0100

    Add a test run for vibe-core 2.x.x.

--- run-ci.sh
+++ run-ci.sh
@@ -6,6 +6,12 @@ DUB_ARGS="--build-mode=${DUB_BUILD_MODE:-separate} ${DUB_ARGS:-}"
 # default to run all parts
 : ${PARTS:=lint,builds,unittests,examples,tests,meson}
 
+# force selecting vibe-core 2.x.x
+if [[ $PARTS =~ (^|,)vibe-core-2(,|$) ]]; then
+    RECIPES=`find | grep dub.sdl`
+    sed -i "s/\"vibe-core\" version=\">=1\.0\.0 <3\.0\.0-0\"/\"vibe-core\" version=\">=2.0.0-0 <3.0.0-0\"/g" $RECIPES
+fi
+
 if [[ $PARTS =~ (^|,)lint(,|$) ]]; then
     ./scripts/test_version.sh
     # Check for trailing whitespace"
