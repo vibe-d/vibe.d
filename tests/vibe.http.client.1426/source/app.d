2fd96821fb4678d3f3d03010d7abfab4aed2bd78
Sönke Ludwig <sludwig@outerproduct.org>
2024-02-14 21:14:00 +0100

    Fix test case for different Windows sockets behavior.

--- tests/vibe.http.client.1426/source/app.d
+++ tests/vibe.http.client.1426/source/app.d
@@ -9,8 +9,11 @@ extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
 int main ()
 {
 	immutable serverAddr = listenTCP(0, (TCPConnection c) @safe nothrow {
-		try c.write("HTTP/1.1 200 OK\r\nConnection: Close\r\n\r\nqwerty");
-		catch (Exception e) assert(0, e.msg);
+		try {
+			// skip request
+			c.readUntil(cast(immutable(ubyte)[])"\r\n\r\n");
+			c.write("HTTP/1.1 200 OK\r\nConnection: Close\r\n\r\nqwerty");
+		} catch (Exception e) assert(0, e.msg);
 	}, "127.0.0.1").bindAddress;
 
 	runTask({
